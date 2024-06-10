71a2f1a80179b65f72933dc0b1e8e6352c46f521
zhuzixian <zhuzixian@gmail.com>
2024-06-10 21:43:40 +0800

    daily dev

---
+++ src/WPF/Controls/SnowyRiver.WPF.Controls.Diagram/Connector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnowyRiver.WPF.Controls.Diagram
+{
+    public class Connector : Control,INotifyPropertyChanged
+    {
+        static Connector()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Connector), new FrameworkPropertyMetadata(typeof(Connector)));
+        }
+
+
+        private List<Connection>? _connections;
+        public List<Connection> Connections
+            => _connections ??= [];
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    internal struct ConnectorInfo
+    {
+        public double DesignerItemLeft { get; set; }
+        public double DesignerItemTop { get; set; }
+        public Size DesignerItemSize { get; set; }
+        public Point Position { get; set; }
+        public ConnectorOrientation Orientation { get; set; }
+    }
+
+    public enum ConnectorOrientation
+    {
+        None,
+        Left,
+        Top,
+        Right,
+        Bottom
+    }
+}

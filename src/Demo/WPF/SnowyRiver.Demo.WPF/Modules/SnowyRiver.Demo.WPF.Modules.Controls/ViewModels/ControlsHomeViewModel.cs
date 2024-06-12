e83faa6e789d76e8aac4267326957b9288ea79bd
zhuzixian <zhuzixian@gmail.com>
2024-06-12 21:35:49 +0800

    daily dev

--- src/Demo/WPF/SnowyRiver.Demo.WPF/Modules/SnowyRiver.Demo.WPF.Modules.Controls/ViewModels/ControlsHomeViewModel.cs
+++ src/Demo/WPF/SnowyRiver.Demo.WPF/Modules/SnowyRiver.Demo.WPF.Modules.Controls/ViewModels/ControlsHomeViewModel.cs
@@ -1,16 +1,38 @@
-﻿using Prism.Commands;
+﻿using System.Drawing;
+using System.Windows.Media;
+using AIStudio.Wpf.DiagramDesigner;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Prism.Regions;
+using SnowyRiver.Demo.WPF.Core.Mvvm;
+using SnowyRiver.WPF.Controls.Diagram;
+using SnowyRiver.WPF.Controls.Valves;
+using Size = System.Windows.Size;
 
-namespace SnowyRiver.Demo.WPF.Modules.Controls.ViewModels
+namespace SnowyRiver.Demo.WPF.Modules.Controls.ViewModels;
+public class ControlsHomeViewModel : RegionViewModelBase
 {
-	public class ControlsHomeViewModel : BindableBase
-	{
-        public ControlsHomeViewModel()
-        {
+    public ControlsHomeViewModel(IRegionManager regionManager):base(regionManager)
+    {
+        DiagramVm.DiagramOption.LayoutOption.PageSizeType = PageSizeType.Custom;
+        DiagramVm.DiagramOption.LayoutOption.PageSize = new Size(double.NaN, double.NaN);
+        DiagramVm.ColorViewModel.FillColor.Color = Colors.Orange;
 
-        }
-	}
+        var threePortsValve = new ThreePortsValve();
+
+        var valve = new ContentDesignerItemViewModel(DiagramVm)
+        {
+            ContentStyle = new ThreePortsValve(),
+            Left = 300, Top = 200,
+        };
+        DiagramVm.Add(valve);
+
+
+    }
+
+    private DiagramViewModel _diagramVm;
+    public DiagramViewModel DiagramVm
+        => _diagramVm ??= new DiagramViewModel
+        {
+            ColorViewModel = new ColorViewModel(),
+        };
 }
---
+++ src/WPF/Controls/SnowyRiver.WPF.Controls.Diagram/ContentDesignerItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using AIStudio.Wpf.DiagramDesigner;
+using AIStudio.Wpf.DiagramDesigner.Models;
+
+namespace SnowyRiver.WPF.Controls.Diagram;
+public class ContentDesignerItemViewModel : DesignerItemViewModelBase
+{
+    public ContentDesignerItemViewModel() : this(null)
+    {
+
+    }
+
+    public ContentDesignerItemViewModel(IDiagramViewModel root) : base(root)
+    {
+
+    }
+
+    public ContentDesignerItemViewModel(IDiagramViewModel root, SelectableItemBase designer) : base(root, designer)
+    {
+
+    }
+
+    public ContentDesignerItemViewModel(IDiagramViewModel root, SerializableItem serializableItem, string serializableType) : base(root, serializableItem, serializableType)
+    {
+    }
+
+    private Style _contentStyle;
+    public Style ContentStyle
+    {
+        get => _contentStyle;
+        set => SetProperty(ref _contentStyle, value);
+    }
+}

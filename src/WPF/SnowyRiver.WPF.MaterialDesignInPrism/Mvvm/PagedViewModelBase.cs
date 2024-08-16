caacbca3f5e160e5acb04f7fed6269016421eb11
zhuzixian <zhuzixian@gmail.com>
2024-08-16 14:21:05 +0800

    增加PagedViewModelBase

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Mvvm/PagedViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Regions;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Mvvm;
+public class PagedViewModelBase(RegionManager regionManager):RegionViewModelBase(regionManager)
+{
+    private long _totalCount;
+    public long TotalCount
+    {
+        get => _totalCount;
+        set
+        {
+            if (SetProperty(ref _totalCount, value))
+            {
+                RaisePropertyChanged(nameof(TotalPage));
+            }
+        }
+    }
+
+    private int _currentPage = 1;
+    public int CurrentPage
+    {
+        get => _currentPage;
+        set => SetProperty(ref _currentPage, value);
+    }
+
+    public int TotalPage => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    private int _pageSize = 100;
+    public virtual int PageSize
+    {
+        get => _pageSize;
+        set
+        {
+            if (SetProperty(ref _pageSize, value))
+            {
+                RaisePropertyChanged(nameof(TotalPage));
+            }
+        }
+    }
+}

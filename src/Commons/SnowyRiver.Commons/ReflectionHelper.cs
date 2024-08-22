f6040aad30576bea2e77bc2cf626736f40a32348
zhuzixian <zhuzixian@gmail.com>
2024-08-22 13:12:57 +0800

    新增SnowyRiver.Commons

---
+++ src/Commons/SnowyRiver.Commons/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System;
+
+namespace SnowyRiver.Commons;
+
+public static class ReflectionHelper
+{
+    public static IEnumerable<Assembly> GetAssembliesByProductName(string productName)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        foreach (var asm in assemblies)
+        {
+            var asmCompanyAttr = asm.GetCustomAttribute<AssemblyProductAttribute>();
+            if (asmCompanyAttr != null && asmCompanyAttr.Product == productName)
+            {
+                yield return asm;
+            }
+        }
+    }
+}

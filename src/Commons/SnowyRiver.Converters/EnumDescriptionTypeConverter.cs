2a4e2325e166eca45abe9af4a48b7dab3f4df2db
zhuzixian <zhuzixian@gmail.com>
2024-09-09 16:34:52 +0800

    添加 SnowyRiver.Converters 项目并重构命名空间
    在 `SnowyRiver.sln` 文件中，添加了一个新的项目 `SnowyRiver.Converters`，并为其配置了调试和发布的构建设置。
    在 `EnumDescriptionTypeConverter.cs` 文件中，重构了类的命名空间和实现，移除了 `IValueConverter` 接口的实现。
    添加了一个新的项目文件 `SnowyRiver.Converters.csproj`，目标框架为 `netstandard2.0`。

---
+++ src/Commons/SnowyRiver.Converters/EnumDescriptionTypeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace SnowyRiver.Converters;
+public class EnumDescriptionTypeConverter(Type type) : EnumConverter(type)
+{
+    public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+    {
+        if (destinationType == typeof(string))
+        {
+            if (value != null)
+            {
+                var fieldInfo = value.GetType().GetField(value.ToString());
+                if (fieldInfo != null)
+                {
+                    var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    return attributes.Length > 0 && !string.IsNullOrEmpty(attributes[0].Description) ? attributes[0].Description : value.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        return base.ConvertTo(context, culture, value, destinationType);
+    }
+}

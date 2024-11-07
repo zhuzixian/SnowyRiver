e1266402fccc00f799fb25ca96b9d173a1ec25dc
zhuzixian <zhuzixian@gmail.com>
2024-11-07 20:55:35 +0800

    增加SnowyRiverApplication类

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/SnowyRiverApplication.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using DryIoc.Microsoft.DependencyInjection;
+using Container = DryIoc.Container;
+using System.Windows;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism;
+public abstract class SnowyRiverApplication : PrismApplication
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        if (!IsSupportMultiProcess)
+        {
+            var thisIsOnlyProcess = GetCurrentIsOnlyProcess();
+            if (!thisIsOnlyProcess)
+            {
+                NativeMethods.PostMessage(NativeMethods.HWND_BROADCAST, NativeMethods.WM_SHOWME,
+                    IntPtr.Zero, IntPtr.Zero);
+                Environment.Exit(-1);
+                return;
+            }
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected virtual bool GetCurrentIsOnlyProcess()
+    {
+        var thisProc = Process.GetCurrentProcess();
+        return Process.GetProcessesByName(thisProc.ProcessName).Length <= 1;
+    }
+
+    protected override IContainerExtension CreateContainerExtension()
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        return new DryIocContainerExtension(new Container(CreateContainerRules())
+            .WithDependencyInjectionAdapter(serviceCollection));
+    }
+
+    protected virtual void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    protected virtual bool IsSupportMultiProcess => true;
+}

9ba1c3adde1c51d3199a95d38712f1d5c3360483
zhuzixian <zhuzixian@gmail.com>
2024-11-08 22:33:01 +0800

    添加 SnowyRiver.WPF.Configuration 项目及 JSON 配置类
    在 SnowyRiver.sln 解决方案中添加了一个新的项目 SnowyRiver.WPF.Configuration，并在该项目中添加了两个新的类 JsonConfiguration 和 JsonConfigurationOptions。
    
    在 SnowyRiver.sln 文件中：
    - 添加了 SnowyRiver.WPF.Configuration 项目。
    - 更新了 GlobalSection(SolutionConfigurationPlatforms) 和 GlobalSection(NestedProjects) 部分以包含新项目的配置信息。
    
    在 JsonConfiguration.cs 文件中：
    - 添加了 JsonConfiguration 类，该类继承自 NotifyPropertyChangedObject。
    - 提供了加载、读取、创建和保存 JSON 配置文件的功能，并包含一些事件和方法来处理配置文件的不同状态。
    
    在 JsonConfigurationOptions.cs 文件中：
    - 添加了 JsonConfigurationOptions 类，用于配置 JSON 序列化和反序列化的选项。
    - 包含一些属性，如 CreateNew 和 SaveNew，以及一个 JsonSerializerOptions 对象，用于自定义 JSON 序列化设置。
    
    在 SnowyRiver.WPF.Configuration.csproj 文件中：
    - 定义了 SnowyRiver.WPF.Configuration 项目的基本属性和依赖项。
    - 该项目使用 netstandard2.0 作为目标框架，并引用了 SnowyRiver.WPF.NotifyPropertyChangedBase 和 System.Text.Json 包。

---
+++ src/WPF/SnowyRiver.WPF. Configuration/JsonConfiguration.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SnowyRiver.WPF.NotifyPropertyChangedBase;
+
+namespace SnowyRiver.WPF.Configuration;
+public abstract class JsonConfiguration:NotifyPropertyChangedObject
+{
+    [JsonIgnore] 
+    public string Path { get; set; } = string.Empty;
+
+    protected virtual string DefaultPath => $"Configs/{GetType().Name}.json";
+
+    [JsonIgnore] 
+    public JsonConfigurationOptions Options { get; set; } = new();
+
+    protected virtual JsonConfigurationOptions DefaultOptions => GlobalOptions;
+
+    public static JsonConfigurationOptions GlobalOptions { get; set; } = new();
+
+    [JsonIgnore]
+    public string Json => JsonSerializer.Serialize(this, GetType());
+
+    public event Action<JsonConfiguration>? Reading;
+    public event Action<JsonConfiguration>? Creating;
+    public event Action<JsonConfiguration>? Loaded;
+    public event Action<JsonConfiguration>? BeforeSave;
+    public event Action<JsonConfiguration>? AfterSave;
+
+    protected virtual void OnReading() => Reading?.Invoke(this);
+
+    protected virtual void OnCreating() => Creating?.Invoke(this);
+
+    protected virtual void OnLoaded() => Loaded?.Invoke(this);
+
+    protected virtual void OnBeforeSave() => BeforeSave?.Invoke(this);
+
+    protected virtual void OnAfterSave() => AfterSave?.Invoke(this);
+
+    public static T? Load<T>() where T : JsonConfiguration, new() => Load<T>(new T().DefaultPath, new T().DefaultOptions);
+    public static T? Load<T>(string path) where T : JsonConfiguration, new() => Load<T>(path, new T().DefaultOptions);
+    public static T? Load<T>(JsonConfigurationOptions options) where T : JsonConfiguration, new() => Load<T>(new T().DefaultPath, options);
+
+    public static T? Load<T>(string path, JsonConfigurationOptions options) where T : JsonConfiguration, new()
+    {
+        T? config = default;
+        if (File.Exists(path))
+            config = Read<T>(path, options);
+        else if (options.CreateNew)
+            config = Create<T>(path, options);
+        config?.OnLoaded();
+        return config;
+    }
+
+    public static T? Read<T>() where T : JsonConfiguration, new() => Read<T>(new T().DefaultPath, new T().DefaultOptions);
+
+    public static T? Read<T>(string path) where T : JsonConfiguration, new() => Read<T>(path, new T().DefaultOptions);
+
+    public static T? Read<T>(JsonConfigurationOptions options) where T : JsonConfiguration, new() => Read<T>(new T().DefaultPath, options);
+
+    public static T? Read<T>(string path, JsonConfigurationOptions options) where T : JsonConfiguration, new()
+    {
+        string json = File.ReadAllText(path);
+        T? config = JsonSerializer.Deserialize<T>(json, options.SerializerOptions);
+        if (config != null)
+        {
+            config.OnReading();
+            config.Path = path;
+            config.Options = options;
+        }
+        return config;
+    }
+
+    public static T Create<T>() where T : JsonConfiguration, new() => Create<T>(new T().DefaultPath, new T().DefaultOptions);
+
+    public static T Create<T>(string path) where T : JsonConfiguration, new() => Create<T>(path, new T().DefaultOptions);
+
+    public static T Create<T>(JsonConfigurationOptions options) where T : JsonConfiguration, new() => Create<T>(new T().DefaultPath, options);
+
+    public static T Create<T>(string path, JsonConfigurationOptions options) where T : JsonConfiguration, new()
+    {
+        T config = new();
+        config.OnCreating();
+        config.Path = path;
+        config.Options = options;
+        if (options.SaveNew)
+            config.Save();
+        return config;
+    }
+
+    public void Save() => Save(Path, Options);
+    public void Save(string path) => Save(path, Options);
+    public void Save(JsonConfigurationOptions options) => Save(Path, options);
+    public void Save(string path, JsonConfigurationOptions options)
+    {
+        OnBeforeSave();
+        string json = JsonSerializer.Serialize(this, GetType(), options.SerializerOptions);
+        File.WriteAllText(path, json);
+        OnAfterSave();
+    }
+
+    public override string ToString() => Json;
+}
---
+++ src/WPF/SnowyRiver.WPF. Configuration/JsonConfigurationOptions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SnowyRiver.WPF.Configuration;
+public class JsonConfigurationOptions
+{
+    public JsonConfigurationOptions() { }
+
+    public JsonConfigurationOptions(JsonConfigurationOptions options)
+    {
+        CreateNew = options.CreateNew;
+        SaveNew = options.SaveNew;
+        SerializerOptions = options.SerializerOptions;
+    }
+
+    /// <summary>
+    /// Gets or sets a <see langword="bool"/> indicating whether to create and return a new config if the config file does not exist.
+    /// </summary>
+    public bool CreateNew { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a <see langword="bool"/> indicating whether to save the newly created config immediately.
+    /// </summary>
+    public bool SaveNew { get; set; } = true;
+
+    public JsonSerializerOptions SerializerOptions { get; set; } = new()
+    {
+        AllowTrailingCommas = true,
+        Converters = { new JsonStringEnumConverter() },
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals | JsonNumberHandling.AllowReadingFromString,
+        PropertyNameCaseInsensitive = true,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+        WriteIndented = true
+    };
+}

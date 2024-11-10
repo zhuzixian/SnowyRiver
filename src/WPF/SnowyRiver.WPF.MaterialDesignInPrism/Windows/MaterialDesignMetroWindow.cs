9b361f081ac60ffd1d4736bcdd24a57696777301
zhuzixian <zhuzixian@gmail.com>
2024-11-10 21:58:37 +0800

    更新项目版本并新增 MaterialDesignMetroWindow 类
    将项目版本从 3.0.1 更新到 3.0.2，并添加对 MaterialDesignThemes.MahApps 包的引用，版本为 3.1.0。新增 MaterialDesignMetroWindow 类，继承自 MahApps.Metro.Controls.MetroWindow，并在构造函数中设置了窗口的样式、标题前景色、发光刷、字体和窗口过渡效果。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using MahApps.Metro.Controls;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Windows;
+public class MaterialDesignMetroWindow : MetroWindow
+{
+    public MaterialDesignMetroWindow()
+    {
+        Style = FindResource("MaterialDesignWindow") as Style;
+        TitleForeground = FindResource("MaterialDesign.Brush.Primary.Dark.Foreground") as Brush;
+        GlowBrush = FindResource("MahApps.Brushes.Accent") as Brush;
+        FontFamily = new FontFamily("Microsoft YaHei");
+        WindowTransitionsEnabled = false;
+    }
+}

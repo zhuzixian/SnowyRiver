8c43d62973a654a3f893b06901bd8687abeda20c
zhuzixian <zhuzixian@gmail.com>
2024-11-11 10:08:19 +0800

    更新项目版本并添加 MaterialDesignMetroDialogWindow 类
    在 `SnowyRiver.WPF.MaterialDesignInPrism.csproj` 文件中，将项目版本从 `3.0.2` 更新到 `3.0.3`。
    在 `MaterialDesignMetroDialogWindow.cs` 文件中，添加了一个新的命名空间 `SnowyRiver.WPF.MaterialDesignInPrism.Windows`。
    新增了 `MaterialDesignMetroDialogWindow` 类，该类继承自 `MaterialDesignMetroWindow` 并实现了 `IDialogWindow` 接口。
    在 `MaterialDesignMetroDialogWindow` 类中，添加了一个可空的 `IDialogResult` 属性，名为 `Result`。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroDialogWindow.cs
@@ -0,0 +1,5 @@
+﻿namespace SnowyRiver.WPF.MaterialDesignInPrism.Windows;
+public class MaterialDesignMetroDialogWindow : MaterialDesignMetroWindow,IDialogWindow
+{
+    public IDialogResult? Result { get; set; }
+}

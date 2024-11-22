22c4669b11e335729d3c26d3989b86dc10d29cc3
zhuzixian <zhuzixian@gmail.com>
2024-11-22 14:49:06 +0800

    更新项目版本并添加窗口启动位置设置
    在 `SnowyRiver.WPF.MaterialDesignInPrism.csproj` 文件中，将项目的版本从 `9.0.4` 更新到 `9.0.5`。
    在 `MaterialDesignMetroDialogWindow.cs` 文件中：
    - 添加了 `using System.Windows;` 语句。
    - 在 `MaterialDesignMetroDialogWindow` 类中添加了一个构造函数，该构造函数将 `WindowStartupLocation` 设置为 `WindowStartupLocation.CenterScreen`。

--- src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroDialogWindow.cs
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroDialogWindow.cs
@@ -1,5 +1,12 @@
-﻿namespace SnowyRiver.WPF.MaterialDesignInPrism.Windows;
+﻿using System.Windows;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Windows;
 public class MaterialDesignMetroDialogWindow : MaterialDesignMetroWindow,IDialogWindow
 {
+    public MaterialDesignMetroDialogWindow()
+    {
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+    }
+
     public IDialogResult? Result { get; set; }
 }

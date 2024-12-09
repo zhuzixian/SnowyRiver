47e9136b0a47e4f34940b2d6492ce4c1da93678f
zhuzixian <zhuzixian@gmail.com>
2024-12-09 22:08:09 +0800

    增加啊ToolBarIcoTextButton

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Controls/ToolBarIcoTextButton.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using MaterialDesignThemes.Wpf;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Controls;
+
+/// <summary>
+/// ToolBarButton.xaml 的交互逻辑
+/// </summary>
+public partial class ToolBarIcoTextButton
+{
+    public ToolBarIcoTextButton()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty KindProperty
+        = DependencyProperty.Register(nameof(Kind), typeof(PackIconKind), typeof(ToolBarIcoTextButton), 
+            new PropertyMetadata(default(PackIconKind)));
+
+    public PackIconKind Kind
+    {
+        get => (PackIconKind)GetValue(KindProperty);
+        set => SetValue(KindProperty, value);
+    }
+
+
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+        nameof(Text), typeof(string), typeof(ToolBarIcoTextButton), new PropertyMetadata(default(string)));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+}

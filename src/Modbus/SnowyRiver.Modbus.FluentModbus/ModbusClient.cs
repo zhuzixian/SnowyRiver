e4bdcaff3b171486491bc927b32cd8e74141e45e
zhuzixian <zhuzixian@gmail.com>
2024-12-18 10:43:03 +0800

    添加 Modbus 项目及其重试策略实现
    在 SnowyRiver.sln 解决方案文件中添加了两个新项目：Modbus 和 SnowyRiver.Modbus.FluentModbus。为 SnowyRiver.Modbus.FluentModbus 项目添加了调试和发布配置，并将其嵌套在 Modbus 项目中。新增 RetryModbusClient 类，使用 FluentModbus 和 Polly 库实现带有重试策略的 Modbus 客户端。添加了 SnowyRiver.Modbus.FluentModbus.csproj 项目文件，目标框架为 .NET 9.0。

---
+++ src/Modbus/SnowyRiver.Modbus.FluentModbus/ModbusClient.cs
@@ -0,0 +1,118 @@
+﻿using FluentModbus;
+using Polly.Retry;
+
+namespace SnowyRiver.Modbus.FluentModbus;
+
+public class RetryModbusClient(ModbusClient client, AsyncRetryPolicy retryPolicy)
+{
+    public ModbusClient BaseClient => client;
+
+    public Task<Memory<T>> ReadHoldingRegistersAsync<T>(int unitIdentifier, int startingAddress, int count,
+        CancellationToken cancellationToken = default) where T : unmanaged
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.ReadHoldingRegistersAsync<T>(unitIdentifier, startingAddress, count, cancellationToken));
+    }
+
+    public Task<Memory<byte>> ReadHoldingRegistersAsync(byte unitIdentifier, ushort startingAddress,
+        ushort quantity, CancellationToken cancellationToken = default)
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.ReadHoldingRegistersAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
+    }
+
+    public Task WriteMultipleRegistersAsync<T>(int unitIdentifier, int startingAddress, T[] dataset,
+        CancellationToken cancellationToken = default) where T : unmanaged
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.WriteMultipleRegistersAsync(unitIdentifier, startingAddress, dataset, cancellationToken));
+    }
+
+    public Task WriteMultipleRegistersAsync(byte unitIdentifier, ushort startingAddress, byte[] dataset,
+        CancellationToken cancellationToken = default)
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.WriteMultipleRegistersAsync(unitIdentifier, startingAddress, dataset, cancellationToken));
+    }
+
+    public Task<Memory<byte>> ReadCoilsAsync(int unitIdentifier, int startingAddress, int quantity,
+        CancellationToken cancellationToken = default)
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.ReadCoilsAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
+    }
+
+    public Task<Memory<byte>> ReadDiscreteInputsAsync(int unitIdentifier, int startingAddress, int quantity,
+        CancellationToken cancellationToken = default)
+    {
+        return retryPolicy.ExecuteAsync(() =>
+            client.ReadDiscreteInputsAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
+    }
+
+    public async Task<Memory<T>> ReadInputRegistersAsync<T>(int unitIdentifier, int startingAddress, int count,
+        CancellationToken cancellationToken = default) where T : unmanaged
+    {
+        return await retryPolicy.ExecuteAsync(() =>
+            client.ReadInputRegistersAsync<T>(unitIdentifier, startingAddress, count, cancellationToken));
+    }
+
+    public async Task<Memory<byte>> ReadInputRegistersAsync(byte unitIdentifier, ushort startingAddress,
+        ushort quantity, CancellationToken cancellationToken = default)
+    {
+        return await retryPolicy.ExecuteAsync(() =>
+            client.ReadInputRegistersAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
+    }
+
+    public async Task WriteSingleCoilAsync(int unitIdentifier, int registerAddress, bool value,
+        CancellationToken cancellationToken = default)
+    {
+        await retryPolicy.ExecuteAsync(() =>
+            client.WriteSingleCoilAsync(unitIdentifier, registerAddress, value, cancellationToken));
+    }
+
+    public async Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, short value,
+        CancellationToken cancellationToken = default)
+    {
+        await retryPolicy.ExecuteAsync(() =>
+            client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
+    }
+
+    public async Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, ushort value,
+        CancellationToken cancellationToken = default)
+    {
+        await retryPolicy.ExecuteAsync(() =>
+            client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
+    }
+
+    public async Task WriteSingleRegisterAsync(byte unitIdentifier, ushort registerAddress, byte[] value,
+        CancellationToken cancellationToken = default)
+    {
+        await retryPolicy.ExecuteAsync(() =>
+            client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
+    }
+
+    public async Task WriteMultipleCoilsAsync(int unitIdentifier, int startingAddress, bool[] values,
+        CancellationToken cancellationToken = default)
+    {
+        await retryPolicy.ExecuteAsync(() => Task.Run(() => 
+            client.WriteMultipleCoilsAsync(unitIdentifier, startingAddress, values, cancellationToken), cancellationToken));
+    }
+
+    public async Task<Memory<TRead>> ReadWriteMultipleRegistersAsync<TRead, TWrite>(int unitIdentifier,
+        int readStartingAddress, int readCount, int writeStartingAddress, TWrite[] dataset,
+        CancellationToken cancellationToken = default) where TRead : unmanaged
+        where TWrite : unmanaged
+    {
+        return await retryPolicy.ExecuteAsync(() =>
+            client.ReadWriteMultipleRegistersAsync<TRead, TWrite>(unitIdentifier, readStartingAddress, readCount,
+                writeStartingAddress, dataset, cancellationToken));
+    }
+
+    public async Task<Memory<byte>> ReadWriteMultipleRegistersAsync(byte unitIdentifier, ushort readStartingAddress,
+        ushort readQuantity, ushort writeStartingAddress, byte[] dataset, CancellationToken cancellationToken = default)
+    {
+        return await retryPolicy.ExecuteAsync(() =>
+            client.ReadWriteMultipleRegistersAsync(unitIdentifier, readStartingAddress, readQuantity, writeStartingAddress,
+                dataset, cancellationToken));
+    }
+}

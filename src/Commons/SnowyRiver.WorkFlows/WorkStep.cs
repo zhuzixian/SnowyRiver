5e99e855a84c2f5635ae72cb715accea0c275e90
zhuzixian <zhuzixian@gmail.com>
2024-12-19 16:01:49 +0800

    添加 SnowyRiver.WorkFlows 项目及相关类和枚举
    在 SnowyRiver.sln 中添加了新的 SnowyRiver.WorkFlows 项目，并配置了调试和发布设置。新增了 SnowyRiver.WorkFlows.csproj 文件，定义了一个目标框架为 net9.0 的 .NET 项目，并引用了 SnowyRiver.WPF.NotifyPropertyChangedBase 项目。
    
    新增了 WorkStep.cs 文件，定义了 SnowyRiver.WorkFlows 命名空间下的 WorkStep 类，该类继承自 NotifyPropertyChangedObject，并包含多个实现通知属性更改的属性（SortId、Name、State、StartTime、EndTime）。
    
    新增了 WorkStepState.cs 文件，定义了 SnowyRiver.WorkFlows 命名空间下的 WorkStepState 枚举，包含 Waiting、Running、Paused、Cancelled、Skipped、Finished、Failed 等状态。

---
+++ src/Commons/SnowyRiver.WorkFlows/WorkStep.cs
@@ -0,0 +1,40 @@
+﻿using SnowyRiver.WPF.NotifyPropertyChangedBase;
+
+namespace SnowyRiver.WorkFlows;
+public class WorkStep : NotifyPropertyChangedObject
+{
+    private int _sortId;
+    public int SortId
+    {
+        get => _sortId;
+        set => Set(ref _sortId, value);
+    }
+
+    private string _name = string.Empty;
+    public string Name
+    {
+        get => _name;
+        set => Set(ref _name, value);
+    }
+
+    private WorkStepState _state;
+    public WorkStepState State
+    {
+        get => _state;
+        set => Set(ref _state, value);
+    }
+
+    private DateTime? _startTime;
+    public DateTime? StartTime
+    {
+        get => _startTime;
+        set => Set(ref _startTime, value);
+    }
+
+    private DateTime? _endTime;
+    public DateTime? EndTime
+    {
+        get => _endTime;
+        set => Set(ref _endTime, value);
+    }
+}

7aa08aabcf9837bd73f2bec301fd187219b76720
zhuzixian <zhuzixian@gmail.com>
2024-12-23 14:37:38 +0800

    添加异步锁定和重试策略，更新项目版本
    在 `ModbusClient.cs` 文件中：
    - 添加 `SemaphoreSlim` 实例 `AsyncLocker` 用于异步锁定。
    - 添加 `ExecuteAsync` 方法，确保操作线程安全并应用重试策略。
    - 修改方法调用，统一使用 `ExecuteAsync` 方法。
    
    在 `SnowyRiver.Modbus.FluentModbus.csproj` 文件中：
    - 增加 `<Version>1.0.1</Version>` 属性，更新项目版本号。

--- src/Modbus/SnowyRiver.Modbus.FluentModbus/ModbusClient.cs
+++ src/Modbus/SnowyRiver.Modbus.FluentModbus/ModbusClient.cs
@@ -7,111 +7,145 @@ public class RetryModbusClient(ModbusClient client, AsyncRetryPolicy retryPolicy
 {
     public ModbusClient BaseClient => client;
 
+    protected SemaphoreSlim AsyncLocker = new(1);
+
+    public Task ExecuteAsync(Func<Task> action)
+    {
+        return retryPolicy.ExecuteAsync(async () =>
+        {
+            await AsyncLocker.WaitAsync();
+            try
+            {
+                await action();
+            }
+            finally
+            {
+                AsyncLocker.Release();
+            }
+        });
+    }
+
+    protected Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> action)
+    {
+        return retryPolicy.ExecuteAsync(async () =>
+        {
+            await AsyncLocker.WaitAsync();
+            try
+            {
+                return await action();
+            }
+            finally
+            {
+                AsyncLocker.Release();
+            }
+        });
+    }
+
     public Task<Memory<T>> ReadHoldingRegistersAsync<T>(int unitIdentifier, int startingAddress, int count,
         CancellationToken cancellationToken = default) where T : unmanaged
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadHoldingRegistersAsync<T>(unitIdentifier, startingAddress, count, cancellationToken));
     }
 
     public Task<Memory<byte>> ReadHoldingRegistersAsync(byte unitIdentifier, ushort startingAddress,
         ushort quantity, CancellationToken cancellationToken = default)
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadHoldingRegistersAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
     }
 
     public Task WriteMultipleRegistersAsync<T>(int unitIdentifier, int startingAddress, T[] dataset,
         CancellationToken cancellationToken = default) where T : unmanaged
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteMultipleRegistersAsync(unitIdentifier, startingAddress, dataset, cancellationToken));
     }
 
     public Task WriteMultipleRegistersAsync(byte unitIdentifier, ushort startingAddress, byte[] dataset,
         CancellationToken cancellationToken = default)
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteMultipleRegistersAsync(unitIdentifier, startingAddress, dataset, cancellationToken));
     }
 
     public Task<Memory<byte>> ReadCoilsAsync(int unitIdentifier, int startingAddress, int quantity,
         CancellationToken cancellationToken = default)
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadCoilsAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
     }
 
     public Task<Memory<byte>> ReadDiscreteInputsAsync(int unitIdentifier, int startingAddress, int quantity,
         CancellationToken cancellationToken = default)
     {
-        return retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadDiscreteInputsAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
     }
 
-    public async Task<Memory<T>> ReadInputRegistersAsync<T>(int unitIdentifier, int startingAddress, int count,
+    public Task<Memory<T>> ReadInputRegistersAsync<T>(int unitIdentifier, int startingAddress, int count,
         CancellationToken cancellationToken = default) where T : unmanaged
     {
-        return await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadInputRegistersAsync<T>(unitIdentifier, startingAddress, count, cancellationToken));
     }
 
-    public async Task<Memory<byte>> ReadInputRegistersAsync(byte unitIdentifier, ushort startingAddress,
+    public Task<Memory<byte>> ReadInputRegistersAsync(byte unitIdentifier, ushort startingAddress,
         ushort quantity, CancellationToken cancellationToken = default)
     {
-        return await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadInputRegistersAsync(unitIdentifier, startingAddress, quantity, cancellationToken));
     }
 
-    public async Task WriteSingleCoilAsync(int unitIdentifier, int registerAddress, bool value,
+    public Task WriteSingleCoilAsync(int unitIdentifier, int registerAddress, bool value,
         CancellationToken cancellationToken = default)
     {
-        await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteSingleCoilAsync(unitIdentifier, registerAddress, value, cancellationToken));
     }
 
-    public async Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, short value,
+    public Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, short value,
         CancellationToken cancellationToken = default)
     {
-        await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
     }
 
-    public async Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, ushort value,
+    public Task WriteSingleRegisterAsync(int unitIdentifier, int registerAddress, ushort value,
         CancellationToken cancellationToken = default)
     {
-        await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
     }
 
-    public async Task WriteSingleRegisterAsync(byte unitIdentifier, ushort registerAddress, byte[] value,
+    public Task WriteSingleRegisterAsync(byte unitIdentifier, ushort registerAddress, byte[] value,
         CancellationToken cancellationToken = default)
     {
-        await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.WriteSingleRegisterAsync(unitIdentifier, registerAddress, value, cancellationToken));
     }
 
-    public async Task WriteMultipleCoilsAsync(int unitIdentifier, int startingAddress, bool[] values,
+    public Task WriteMultipleCoilsAsync(int unitIdentifier, int startingAddress, bool[] values,
         CancellationToken cancellationToken = default)
     {
-        await retryPolicy.ExecuteAsync(() => Task.Run(() => 
+        return ExecuteAsync(() => Task.Run(() => 
             client.WriteMultipleCoilsAsync(unitIdentifier, startingAddress, values, cancellationToken), cancellationToken));
     }
 
-    public async Task<Memory<TRead>> ReadWriteMultipleRegistersAsync<TRead, TWrite>(int unitIdentifier,
+    public Task<Memory<TRead>> ReadWriteMultipleRegistersAsync<TRead, TWrite>(int unitIdentifier,
         int readStartingAddress, int readCount, int writeStartingAddress, TWrite[] dataset,
         CancellationToken cancellationToken = default) where TRead : unmanaged
         where TWrite : unmanaged
     {
-        return await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadWriteMultipleRegistersAsync<TRead, TWrite>(unitIdentifier, readStartingAddress, readCount,
                 writeStartingAddress, dataset, cancellationToken));
     }
 
-    public async Task<Memory<byte>> ReadWriteMultipleRegistersAsync(byte unitIdentifier, ushort readStartingAddress,
+    public Task<Memory<byte>> ReadWriteMultipleRegistersAsync(byte unitIdentifier, ushort readStartingAddress,
         ushort readQuantity, ushort writeStartingAddress, byte[] dataset, CancellationToken cancellationToken = default)
     {
-        return await retryPolicy.ExecuteAsync(() =>
+        return ExecuteAsync(() =>
             client.ReadWriteMultipleRegistersAsync(unitIdentifier, readStartingAddress, readQuantity, writeStartingAddress,
                 dataset, cancellationToken));
     }

9310f4fc8cf465ada42bc237a4339e8e1f006fcb
Shady Joo <zhuzixian@gmail.com>
2025-01-06 10:42:56 +0800

    重构工作流和工作步骤类，更新版本号至1.0.7
    更新 `SnowyRiver.WorkFlows.csproj` 文件中的版本号至 `1.0.7`。
    在 `WorkFlow.cs` 文件中：
      * 移除不必要的 `using` 语句。
      * 修改 `WorkFlow` 类以继承新的 `WorkFlow<TStep>` 基类，并调整泛型参数顺序。
      * 移除 `Name`、`Steps`、`StartTime`、`EndTime` 和 `State` 属性。
    在 `WorkStep.cs` 文件中：
      * 移除不必要的 `using` 语句。
      * 修改 `WorkStep` 类以继承新的 `WorkStep` 基类。
      * 移除 `SortId`、`Enable`、`Name`、`State`、`StartTime` 和 `EndTime` 属性。
    新增 `WorkFlowBase.cs` 文件：
      * 引入必要的命名空间。
      * 定义新的 `WorkFlow<TStep>` 基类，包含 `Name`、`Steps`、`StartTime`、`EndTime` 和 `State` 属性。
    新增 `WorkStepBase.cs` 文件：
      * 引入必要的命名空间。
      * 定义新的 `WorkStep` 基类，包含 `SortId`、`Enable`、`Name`、`State`、`StartTime` 和 `EndTime` 属性。

--- src/Commons/SnowyRiver.WorkFlows/WorkFlow.cs
+++ src/Commons/SnowyRiver.WorkFlows/WorkFlow.cs
@@ -1,8 +1,5 @@
-﻿using SnowyRiver.WPF.NotifyPropertyChangedBase;
-using System.Collections.ObjectModel;
-
-namespace SnowyRiver.WorkFlows;
-public class WorkFlow<TStep, TKey, TStepKey>: NotifyPropertyChangedObject
+﻿namespace SnowyRiver.WorkFlows;
+public class WorkFlow<TKey, TStep, TStepKey>: WorkFlow<TStep>
      where TStep : WorkStep<TStepKey>
 {
     private TKey _id;
@@ -10,41 +7,5 @@ public class WorkFlow<TStep, TKey, TStepKey>: NotifyPropertyChangedObject
     {
         get => _id;
         set => Set(ref _id, value);
-    }
-
-    private string _name = string.Empty;
-    public string Name
-    {
-        get => _name;
-        set => Set(ref _name, value);
-    }
-
-    private ObservableCollection<TStep> _steps = [];
-
-    public ObservableCollection<TStep> Steps
-    {
-        get => _steps;
-        set => Set(ref _steps, value);
-    }
-
-    private DateTime? _startTime;
-    public DateTime? StartTime
-    {
-        get => _startTime;
-        set => Set(ref _startTime, value);
-    }
-
-    private DateTime? _endTime;
-    public DateTime? EndTime
-    {
-        get => _endTime;
-        set => Set(ref _endTime, value);
-    }
-
-    private WorkState _state;
-    public WorkState State
-    {
-        get => _state;
-        set => Set(ref _state, value);
     }
 }
--- src/Commons/SnowyRiver.WorkFlows/WorkStep.cs
+++ src/Commons/SnowyRiver.WorkFlows/WorkStep.cs
@@ -1,54 +1,10 @@
-﻿using SnowyRiver.WPF.NotifyPropertyChangedBase;
-
-namespace SnowyRiver.WorkFlows;
-public class WorkStep<TKey>: NotifyPropertyChangedObject
+﻿namespace SnowyRiver.WorkFlows;
+public class WorkStep<TKey>: WorkStep
 {
     private TKey _id;
     public TKey Id
     {
         get => _id;
         set => Set(ref _id, value);
-    }
-
-    private int _sortId;
-    public int SortId
-    {
-        get => _sortId;
-        set => Set(ref _sortId, value);
-    }
-
-    private bool _enable;
-    public bool Enable
-    {
-        get => _enable;
-        set => Set(ref _enable, value);
-    }
-
-    private string _name = string.Empty;
-    public string Name
-    {
-        get => _name;
-        set => Set(ref _name, value);
-    }
-
-    private WorkState _state;
-    public WorkState State
-    {
-        get => _state;
-        set => Set(ref _state, value);
-    }
-
-    private DateTime? _startTime;
-    public DateTime? StartTime
-    {
-        get => _startTime;
-        set => Set(ref _startTime, value);
-    }
-
-    private DateTime? _endTime;
-    public DateTime? EndTime
-    {
-        get => _endTime;
-        set => Set(ref _endTime, value);
     }
 }

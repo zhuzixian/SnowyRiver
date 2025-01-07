89b2d21a47af799054dfa9e8ae01fec70e651139
zhuzixian <zhuzixian@gmail.com>
2025-01-07 20:40:25 +0800

    更换认证服务接口
    在 `LoginViewModel.cs` 中，将 `IAuthenticationServiceBase<User, Team, Role, Permission>` 替换为 `IAuthenticationService`。
    在 `LoginViewModelBase.cs` 中，将 `IAuthenticationServiceBase<TUser, TTeam, TRole, TPermission>` 替换为 `IAuthenticationService<TUser, TTeam, TRole, TPermission>`。
    
    这些更改简化了接口使用，适应了新的认证服务接口定义。

--- src/Accounts/Modules/SnowyRiver.Accounts.Modules.Manager/ViewModels/LoginViewModel.cs
+++ src/Accounts/Modules/SnowyRiver.Accounts.Modules.Manager/ViewModels/LoginViewModel.cs
@@ -4,7 +4,7 @@ using SnowyRiver.Accounts.Modules.Manager.Interfaces.Services;
 
 namespace SnowyRiver.Accounts.Modules.Manager.ViewModels;
 public class LoginViewModel(
-    IAuthenticationServiceBase<User, Team, Role, Permission> authenticationService,
+    IAuthenticationService authenticationService,
     IRegionManager regionManager):LoginViewModel<User, Team, Role, Permission>(authenticationService, regionManager)
 {
 }

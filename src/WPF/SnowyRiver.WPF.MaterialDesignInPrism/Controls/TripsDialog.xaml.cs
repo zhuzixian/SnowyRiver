05a025031cb3630cb5b4c4595a54a88a1d3bdf5f
Shady Joo <zhuzixian@gmail.com>
2025-01-15 12:51:50 +0800

    添加 TripsDialog 控件并更新项目版本
    在 TripsDialog.xaml 中添加新的 UserControl，包括 Grid 布局和三个 TextBlock 及 ItemsControl 元素。新增 TripsDialog 类，定义依赖属性 Title、Message 和 Buttons，并在构造函数中初始化组件。更新 SnowyRiver.WPF.MaterialDesignInPrism.csproj 项目版本至 9.0.17。移除 DialogView.xaml 中原有的 Grid 布局，替换为新的 TripsDialog 控件并绑定属性。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Controls/TripsDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Controls;
+/// <summary>
+/// Interaction logic for TripsDialog
+/// </summary>
+public partial class TripsDialog : UserControl
+{
+    public TripsDialog()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty TileProperty = DependencyProperty.Register(
+        nameof(Title), typeof(string), typeof(TripsDialog), new PropertyMetadata(default(string)));
+
+    public string Title
+    {
+        get => (string)GetValue(TileProperty);
+        set => SetValue(TileProperty, value);
+    }
+
+    public static readonly DependencyProperty MessageProperty = DependencyProperty.Register(
+        nameof(Message), typeof(string), typeof(TripsDialog), new PropertyMetadata(default(string)));
+
+    public string Message
+    {
+        get => (string)GetValue(TileProperty);
+        set => SetValue(TileProperty, value);
+    }
+
+    public static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
+        nameof(Buttons), typeof(IEnumerable), typeof(TripsDialog), new PropertyMetadata(default(IEnumerable)));
+
+    public IEnumerable Buttons
+    {
+        get => (IEnumerable)GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
+    }
+}

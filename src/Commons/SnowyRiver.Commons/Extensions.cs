58efccb92c659206d478fca099ddd60dcec413f0
Shady Joo <zhuzixian@gmail.com>
2025-02-07 16:55:49 +0800

    更新项目版本并新增扩展方法
    在 `SnowyRiver.Commons.csproj` 文件中：
    - 将项目版本从 `1.0.6` 更新到 `1.0.7`。
    - 启用了可空引用类型（`<Nullable>enable</Nullable>`）。
    
    在 `Extensions.cs` 文件中：
    - 新增了 `System.Collections.ObjectModel` 和 `System.Threading.Tasks` 的 `using` 语句。
    - 新增了 `SnowyRiver.Commons` 命名空间。
    - 新增了一个静态类 `Extensions`，其中包含三个扩展方法：
      - `InsertPrevious<T>`：在指定项之前插入一个新项，并更新 `SortId`。
      - `InsertNext<T>`：在指定项之后插入一个新项，并更新 `SortId`。
      - `Remove<T>`：移除指定项，并更新 `SortId`。

---
+++ src/Commons/SnowyRiver.Commons/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace SnowyRiver.Commons;
+public static class Extensions
+{
+    public static void InsertPrevious<T>(this Collection<T> source, T? nullableSelectedItem)
+        where T : IHasSortId, new()
+    {
+        if (nullableSelectedItem != null)
+        {
+            var selectedIndex = source.IndexOf(nullableSelectedItem);
+            source.Insert(selectedIndex, new T());
+            for (var i = selectedIndex; i < source.Count; i++)
+            {
+                if (source[i] is { } t)
+                {
+                    t.SortId = i + 1;
+                }
+            }
+        }
+    }
+
+    public static void InsertNext<T>(this Collection<T> source, T? nullableSelectedItem)
+        where T : IHasSortId, new()
+    {
+        if (nullableSelectedItem != null)
+        {
+            var selectedIndex = source.IndexOf(nullableSelectedItem);
+            source.Insert(selectedIndex + 1, new T());
+            for (var i = selectedIndex; i < source.Count; i++)
+            {
+                if (source[i] is { } t)
+                {
+                    t.SortId = i + 1;
+                }
+            }
+        }
+    }
+
+    public static void Remove<T>(this Collection<T> source, T? nullableSelectedItem)
+        where T : IHasSortId, new()
+    {
+        if (nullableSelectedItem is { } selectedStep)
+        {
+            var selectedIndex = source.IndexOf(selectedStep);
+            source.RemoveAt(selectedIndex);
+            for (var i = selectedIndex; i < source.Count; i++)
+            {
+                if (source[i] is { } t)
+                {
+                    t.SortId = i + 1;
+                }
+            }
+        }
+    }
+}

4c0141c2078cd989c93b82b06284f682b8509ef4
Shady Joo <zhuzixian@gmail.com>
2025-02-10 15:12:48 +0800

    更新 MaterialDesign 版本并添加窗口状态处理
    在 `SnowyRiver.WPF.MaterialDesignInPrism.csproj` 文件中：
    - 将 `<Version>` 从 `9.0.17` 更新为 `9.0.18`。
    
    在 `MaterialDesignMetroWindow.cs` 文件中：
    - 添加 `using System.Windows.Interop;` 引用。
    - 添加私有字段 `_lastNotMinimizedWindowState`。
    - 在构造函数中添加 `StateChanged` 事件处理程序 `MainWindow_OnStateChanged`。
    - 重写 `OnSourceInitialized` 方法，添加 `WndProc` 钩子。
    - 添加 `WndProc` 方法，处理 `WM_SHOWME` 消息并恢复窗口状态。
    - 添加 `MainWindow_OnStateChanged` 方法，记录窗口非最小化状态。
    - 添加虚拟属性 `EnableActivateOnShowMeWindowMessage`，默认返回 `false`。

--- src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroWindow.cs
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Windows/MaterialDesignMetroWindow.cs
@@ -1,12 +1,16 @@
 ﻿using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
 using MahApps.Metro.Controls;
 
 namespace SnowyRiver.WPF.MaterialDesignInPrism.Windows;
 public class MaterialDesignMetroWindow : MetroWindow
 {
+    private WindowState _lastNotMinimizedWindowState;
+
     public MaterialDesignMetroWindow()
     {
+        StateChanged += MainWindow_OnStateChanged;
         Style = FindResource("MaterialDesignWindow") as Style;
         TitleForeground = FindResource("MaterialDesign.Brush.Primary.Dark.Foreground") as Brush;
         GlowBrush = FindResource("MahApps.Brushes.Accent") as Brush;
@@ -14,4 +18,36 @@ public class MaterialDesignMetroWindow : MetroWindow
         WindowTransitionsEnabled = false;
         SetBinding(TitleProperty, "Title");
     }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        var source = PresentationSource.FromVisual(this) as HwndSource;
+        source?.AddHook(WndProc);
+    }
+
+    protected virtual IntPtr WndProc(IntPtr hwnd, int msg,
+        IntPtr wparam, IntPtr lparam, ref bool handled)
+    {
+        if (msg == NativeMethods.WM_SHOWME && EnableActivateOnShowMeWindowMessage)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                WindowState = _lastNotMinimizedWindowState;
+            }
+            Activate();
+        }
+
+        return IntPtr.Zero;
+    }
+
+    private void MainWindow_OnStateChanged(object? sender, EventArgs e)
+    {
+        if (WindowState != WindowState.Minimized)
+        {
+            _lastNotMinimizedWindowState = WindowState;
+        }
+    }
+
+    protected virtual bool EnableActivateOnShowMeWindowMessage => false;
 }

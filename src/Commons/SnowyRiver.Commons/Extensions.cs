0c7235ad0f497b0b228fd9bb1e86307db5c775a8
zhuzixian <zhuzixian@gmail.com>
2025-02-11 22:19:07 +0800

    更新 `Extensions` 方法签名和项目版本号
    更改说明：
    
    * 在 `Extensions.cs` 文件中，`Extensions` 类的 `IsNullOrEmpty` 和 `IsNullOrWhiteSpace` 方法的签名发生了变化。具体来说，删除了泛型参数 `<T>`，使得这两个方法不再是泛型方法。
    * 在 `SnowyRiver.Commons.csproj` 文件中，项目的版本号从 `1.0.9` 更新到了 `1.0.10`。

--- src/Commons/SnowyRiver.Commons/Extensions.cs
+++ src/Commons/SnowyRiver.Commons/Extensions.cs
@@ -68,12 +68,12 @@ public static class Extensions
         }
     }
 
-    public static bool IsNullOrEmpty<T>(this string value)
+    public static bool IsNullOrEmpty(this string value)
     {
         return string.IsNullOrEmpty(value);
     }
 
-    public static bool IsNullOrWhiteSpace<T>(this string value)
+    public static bool IsNullOrWhiteSpace(this string value)
     {
         return string.IsNullOrWhiteSpace(value);
     }

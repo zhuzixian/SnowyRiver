8acd112783867650708493359f57b124185beef2
Shady Joo <zhuzixian@gmail.com>
2025-02-11 14:59:26 +0800

    更新项目配置并添加 ModbusOptions 类
    在 SnowyRiver.Modbus.FluentModbus.csproj 文件中：
    - 将目标框架从 net9.0 改为 net9.0。
    - 将版本号从 1.0.8 更新为 1.0.9。
    - 添加对 SnowyRiver.WPF.Configuration 项目的引用。
    
    在 ModbusOptions.cs 文件中：
    - 添加命名空间引用：System.IO.Ports、FluentModbus、SnowyRiver.WPF.Configuration。
    - 创建命名空间 SnowyRiver.Modbus.FluentModbus。
    - 定义 ModbusOptions 类，继承自 JsonConfiguration。
    - 在 ModbusOptions 类中添加属性：IsMock、PortName、BaudRate、Parity、StopBits、ReadTimeout、WriteTimeout、Endian。

---
+++ src/Modbus/SnowyRiver.Modbus.FluentModbus/ModbusOptions.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Ports;
+using FluentModbus;
+using SnowyRiver.WPF.Configuration;
+
+namespace SnowyRiver.Modbus.FluentModbus;
+public class ModbusOptions : JsonConfiguration
+{
+    public bool IsMock { get; set; }
+
+    private string _portName = string.Empty;
+
+    public string PortName
+    {
+        get => _portName;
+        set => Set(ref _portName, value);
+    }
+
+    private int _baudRate = 115200;
+
+    public int BaudRate
+    {
+        get => _baudRate; 
+        set => Set(ref _baudRate, value);
+    }
+
+    private Parity _parity = Parity.Even;
+    public Parity Parity
+    {
+        get => _parity; 
+        set => Set(ref _parity, value);
+    }
+
+    private StopBits _stopBits = StopBits.One;
+    public StopBits StopBits
+    {
+        get => _stopBits; 
+        set => Set(ref _stopBits, value);
+    }
+
+    private int _readTimeout = 1000;
+
+    public int ReadTimeout
+    {
+        get => _readTimeout;
+        set => Set(ref _readTimeout, value);
+    }
+
+    private int _writeTimeout = 1000;
+    public int WriteTimeout
+    {
+        get => _writeTimeout;
+        set => Set(ref _writeTimeout, value);
+    }
+
+    private ModbusEndianness _endian = ModbusEndianness.LittleEndian;
+    public ModbusEndianness Endian
+    {
+        get => _endian;
+        set => Set(ref _endian, value);
+    }
+}

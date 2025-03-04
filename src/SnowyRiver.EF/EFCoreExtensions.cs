7f563a8f4e469ad7137bab2954777f75c75fc90c
Shady Joo <zhuzixian@gmail.com>
2025-03-04 15:06:49 +0800

    添加 SnowyRiver.Ef 项目及软删除功能
    在解决方案中新增 `SnowyRiver.Ef` 项目，并配置调试和发布设置。
    在 `EFCoreExtensions.cs` 中实现 `EnableSoftDeletionGlobalFilter` 扩展方法，以支持软删除查询筛选器。
    更新 `SnowyRiver.EF.csproj`，设置目标框架为 `net9.0`，启用隐式使用和可空引用类型，并添加 `Microsoft.EntityFrameworkCore` 包引用。

---
+++ src/SnowyRiver.EF/EFCoreExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SnowyRiver.Domain.Shared.Entities;
+
+namespace SnowyRiver.EF;
+public static class EfCoreExtensions
+{
+    // 为每个实体设置全局“IsDeleted=false”查询筛选器
+    public static void EnableSoftDeletionGlobalFilter(this ModelBuilder modelBuilder)
+    {
+        var entityTypesHasSoftDeletion = modelBuilder.Model.GetEntityTypes()
+            .Where(e => e.ClrType.IsAssignableTo(typeof(ISoftDelete)));
+
+        foreach (var entityType in entityTypesHasSoftDeletion)
+        {
+            var isDeletedProperty = entityType.FindProperty(nameof(ISoftDelete.IsDeleted));
+            if (isDeletedProperty?.PropertyInfo != null)
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "p");
+                var filter = Expression.Lambda(
+                    Expression.Not(Expression.Property(parameter, isDeletedProperty.PropertyInfo)),
+                    parameter);
+                entityType.SetQueryFilter(filter); ;
+            }
+        }
+    }
+}

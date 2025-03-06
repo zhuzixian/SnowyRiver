adb1d5fa57a033cfb832827c0a6dca3d047f7292
zhuzixian <zhuzixian@gmail.com>
2025-03-06 20:02:54 +0800

    更新可空字符串支持和版本号
    在 `Extensions.cs` 文件中，`IsNullOrEmpty` 和 `IsNullOrWhiteSpace` 方法的参数类型从 `string` 更改为 `string?`，以支持可空字符串。这使得这些方法能够更好地处理可能为 null 的字符串。
    
    在 `SnowyRiver.Commons.csproj` 文件中，项目的版本号从 `1.0.12` 更新为 `1.0.13`，这通常表示进行了某些更改或修复。

--- src/Commons/SnowyRiver.Commons/Extensions.cs
+++ src/Commons/SnowyRiver.Commons/Extensions.cs
@@ -1,12 +1,12 @@
 ﻿namespace SnowyRiver.Commons;
 public static class Extensions
 {
-    public static bool IsNullOrEmpty(this string value)
+    public static bool IsNullOrEmpty(this string? value)
     {
         return string.IsNullOrEmpty(value);
     }
 
-    public static bool IsNullOrWhiteSpace(this string value)
+    public static bool IsNullOrWhiteSpace(this string? value)
     {
         return string.IsNullOrWhiteSpace(value);
     }

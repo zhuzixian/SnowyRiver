a06d6e75981efdc4842f5091130f2570b5125318
Shady Joo <zhuzixian@gmail.com>
2025-03-28 12:57:51 +0800

    更新版本并重构 ShowDialogAsync 方法
    在 `SnowyRiver.WPF.Modules.Splash.csproj` 文件中，将版本号从 `1.0.13` 更新为 `1.0.14`。
    在 `SplashContentViewModel.cs` 文件中，重构了 `ShowDialogAsync` 方法，改为接受视图名称和导航参数，同时添加了一个重载方法以支持原有的标题、消息和按钮数组功能。此更改增强了方法的灵活性和可读性。

--- src/WPF/Modules/SnowyRiver.WPF.Modules.Splash/ViewModels/SplashContentViewModel.cs
+++ src/WPF/Modules/SnowyRiver.WPF.Modules.Splash/ViewModels/SplashContentViewModel.cs
@@ -8,22 +8,34 @@ namespace SnowyRiver.WPF.Modules.Splash.ViewModels;
 
 public class SplashContentViewModel(IRegionManager regionManager) : RegionViewModelBase(regionManager)
 {
-    protected async Task<string> ShowDialogAsync(string title, string message, string[] buttons)
+    protected async Task<string> ShowDialogAsync(string view, NavigationParameters parameters)
     {
         var dialogResult = new SplashDialogResult();
-        RegionManager.RequestNavigate(RegionNames.SplashContentRegion, ViewNames.DialogView,
-            new NavigationParameters
-            {
-                { nameof(DialogViewModel.Title), title },
-                { nameof(DialogViewModel.Message), message },
-                { nameof(DialogViewModel.Buttons), buttons },
-                { nameof(DialogViewModel), dialogResult }
-            });
+        var navigationParameters = new NavigationParameters
+        {
+            { nameof(DialogViewModel), dialogResult }
+        };
+        foreach (var parameter in parameters)
+        {
+            navigationParameters.Add(parameter.Key, parameter.Value!);
+        }
+        RegionManager.RequestNavigate(RegionNames.SplashContentRegion, view, navigationParameters);
         while (string.IsNullOrEmpty(dialogResult.Value))
         {
             await Task.Delay(TimeSpan.FromMilliseconds(200));
         }
 
         return dialogResult.Value!;
+    }
+
+    protected async Task<string> ShowDialogAsync(string title, string message, string[] buttons)
+    {
+        return await ShowDialogAsync(ViewNames.DialogView,
+            new NavigationParameters
+            {
+                { nameof(DialogViewModel.Title), title },
+                { nameof(DialogViewModel.Message), message },
+                { nameof(DialogViewModel.Buttons), buttons },
+            });
     }
 }

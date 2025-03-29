094ee27e904e206adbf58609f98d54827b9061e7
zhuzixian <zhuzixian@gmail.com>
2025-03-29 19:20:26 +0800

    添加 SnowyRiver.WPF 项目及相关功能
    在解决方案中新增 `SnowyRiver.WPF` 项目，并配置调试和发布设置。
    在 `DependencyObjectExtensionMethods.cs` 中添加 `DependencyObjectExtensionMethods` 类，包含 `FindVisualChild<TChild>` 方法用于查找可视子对象。
    更新 `SnowyRiver.WPF.csproj` 文件，添加项目基本配置，包括目标框架、可空性、WPF 使用、隐式使用和版本号等信息。

---
+++ src/WPF/SnowyRiver.WPF/DependencyObjectExtensionMethods.cs
@@ -0,0 +1,27 @@
+
+using System.Windows;
+using System.Windows.Media;
+
+namespace SnowyRiver.WPF;
+public static class DependencyObjectExtensionMethods
+{
+    public static TChild? FindVisualChild<TChild>(DependencyObject obj) where TChild : DependencyObject
+    {
+        for (var i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+        {
+            var child = VisualTreeHelper.GetChild(obj, i);
+            if (child is TChild tChild)
+            {
+                return tChild;
+            }
+
+            var childOfChildren = FindVisualChild<TChild>(child);
+            if (childOfChildren != null)
+            {
+                return childOfChildren;
+            }
+        }
+
+        return null;
+    }
+}

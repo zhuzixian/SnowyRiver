772293cb2d0cc30cc009b384740c805488a644f1
Shady Joo <zhuzixian@gmail.com>
2025-04-08 15:25:31 +0800

    更新版本并新增加载窗口
    在 `SnowyRiver.WPF.Modules.Splash.csproj` 文件中，将版本号从 `1.0.20` 更新为 `1.0.21`。
    新增 `LoadingWindow.xaml` 文件，定义了一个名为 `LoadingWindow` 的窗口，包含图像、文本块和进度条，并设置了窗口的属性。
    同时新增 `LoadingWindow.xaml.cs` 文件，定义了 `LoadingWindow` 类，并在构造函数中加载 `logo.png` 图像文件。

---
+++ src/WPF/Modules/SnowyRiver.WPF.Modules.Splash/Views/LoadingWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace SnowyRiver.WPF.Modules.Splash.Views
+{
+    /// <summary>
+    /// SplashWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingWindow : Window
+    {
+        public LoadingWindow()
+        {
+            InitializeComponent();
+
+            var logoFileInfo = new FileInfo("./Resources/logo.png");
+            if (logoFileInfo.Exists)
+            {
+                Logo.Source = new BitmapImage(new Uri(logoFileInfo.FullName));
+            }
+        }
+    }
+}

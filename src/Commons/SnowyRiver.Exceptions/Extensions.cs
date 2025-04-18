f85723590fa0d7ce4fe7b192cb8778e044e736f0
Shady Joo <zhuzixian@gmail.com>
2025-04-18 12:04:47 +0800

    添加 SnowyRiver.Exceptions 项目及扩展方法
    在解决方案中新增 `SnowyRiver.Exceptions` 项目，并配置调试和发布设置。
    在 `Extensions.cs` 中实现 `IsTaskCanceledException` 扩展方法，用于检查 `TaskCanceledException`。
    更新 `SnowyRiver.Exceptions.csproj`，指定项目 SDK 和目标框架为 .NET Standard 2.0。

---
+++ src/Commons/SnowyRiver.Exceptions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace SnowyRiver.Exceptions;
+public static class Extensions
+{
+    public static bool IsTaskCanceledException(this Exception e)
+    {
+        if (e is TaskCanceledException)
+        {
+            return true;
+        }
+
+        return e.InnerException != null && IsTaskCanceledException(e.InnerException);
+    }
+}

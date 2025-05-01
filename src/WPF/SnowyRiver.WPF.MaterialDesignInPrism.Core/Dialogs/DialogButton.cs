129407c55aa589c7e467538696d7633eb2133ca5
zhuzixian <zhuzixian@gmail.com>
2025-05-01 22:17:28 +0800

    更新版本并添加对话按钮类
    在 `SnowyRiver.WPF.MaterialDesignInPrism.Core.csproj` 文件中，将版本号从 `1.0.2` 更新为 `1.0.3`。
    在 `DialogButton.cs` 文件中，新增命名空间 `SnowyRiver.WPF.MaterialDesignInPrism.Core.Dialogs`，并添加 `DialogButton` 类，该类包含五个常量字符串，表示确认、取消、中止、重试和继续的中文文本。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism.Core/Dialogs/DialogButton.cs
@@ -0,0 +1,9 @@
+﻿namespace SnowyRiver.WPF.MaterialDesignInPrism.Core.Dialogs;
+public class DialogButton
+{
+    public const string Confirm = "确认";
+    public const string Cancel = "取消";
+    public const string Abort = "中止";
+    public const string TryAgain = "重试";
+    public const string Resume = "继续";
+}

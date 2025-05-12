2af2b3010402f7692ecc1742f83842f7f8d6b627
Shady Joo <zhuzixian@gmail.com>
2025-05-12 17:46:28 +0800

    更新版本并添加闪烁动画类
    在 `SnowyRiver.WPF.csproj` 文件中，将版本号从 `1.0.2` 更新为 `1.0.3`。
    
    在 `BlinkAnimation.cs` 文件中，新增了 `BlinkAnimation` 类，提供闪烁动画功能。该类包含单例实例 `Instance` 和依赖属性 `BlinkOpacity`，构造函数设置了持续 1.5 秒的闪烁动画，并实现了异常处理。

---
+++ src/WPF/SnowyRiver.WPF/Animation/BlinkAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Media.Animation;
+using System.Windows;
+
+namespace SnowyRiver.WPF.Animation;
+public class BlinkAnimation : Animatable
+{
+    /// <summary>
+    /// 单例，保持所有闪烁的动画同步
+    /// </summary>
+    public static readonly BlinkAnimation Instance = new();
+
+    public double BlinkOpacity
+    {
+        get => (double)GetValue(BlinkOpacityProperty);
+        set => SetValue(BlinkOpacityProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for BlinkOpacity.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty BlinkOpacityProperty =
+        DependencyProperty.Register(nameof(BlinkOpacity), typeof(double), typeof(BlinkAnimation), new PropertyMetadata(0.0));
+
+
+
+    /// <summary>
+    /// 闪烁动画，用于动画同步
+    /// </summary>
+    public BlinkAnimation()
+    {
+        try
+        {
+            var doubleAnimation = new DoubleAnimationUsingKeyFrames
+            {
+                RepeatBehavior = RepeatBehavior.Forever,
+                Duration = TimeSpan.FromSeconds(1.5)
+            };
+
+            var frame1 = new DiscreteDoubleKeyFrame(0.0, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0)));
+            var frame2 = new DiscreteDoubleKeyFrame(1.0, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0.75)));
+
+            doubleAnimation.KeyFrames.Add(frame1);
+            doubleAnimation.KeyFrames.Add(frame1);
+
+            BeginAnimation(BlinkOpacityProperty, doubleAnimation);
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+
+    protected override Freezable CreateInstanceCore()
+    {
+        return Instance;
+    }
+}

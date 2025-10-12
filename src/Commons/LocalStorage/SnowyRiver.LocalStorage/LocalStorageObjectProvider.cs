ae04c5092a24e9e648e32930a81dd8f22b6ff00c
Shady Joo <zhuzixian@gmail.com>
2025-10-12 10:25:13 +0800

    添加 LocalStorageObjectProvider<T> 类及相关依赖
    在项目中新增了 LocalStorageObjectProvider<T> 类：
    - 在 `SnowyRiver.LocalStorage.csproj` 中添加了对以下项目的引用：
      - `SnowyRiver.ComponentModel.NotifyPropertyChanged.csproj`
      - `SnowyRiver.Serializer.Interfaces.csproj`
      - `SnowyRiver.LocalStorage.Interface.csproj`
    - 新增的 `LocalStorageObjectProvider<T>` 类继承自 `NotifyPropertyChangedObject` 并实现了 `ILocalStorageObjectProvider<T>` 接口。
      - 支持异步初始化、保存和刷新本地存储对象。
      - 提供了泛型属性 `Object` 和虚拟属性 `Key`。

---
+++ src/Commons/LocalStorage/SnowyRiver.LocalStorage/LocalStorageObjectProvider.cs
@@ -0,0 +1,35 @@
+﻿using SnowyRiver.ComponentModel.NotifyPropertyChanged;
+using SnowyRiver.LocalStorage.Interface;
+
+namespace SnowyRiver.LocalStorage;
+public class LocalStorageObjectProvider<T>(
+    ILocalStorageService localStorageService)
+    :NotifyPropertyChangedObject, ILocalStorageObjectProvider<T>
+{
+    public async Task InitializeAsync(CancellationToken cancellationToken = default)
+    {
+        await RefreshAsync(cancellationToken);
+    }
+
+    public async Task SaveAsync(CancellationToken cancellationToken = default)
+    {
+        await localStorageService.SetItemAsync(Key, Object, cancellationToken);
+    }
+
+    public async Task RefreshAsync(CancellationToken cancellationToken = default)
+    {
+        Object = await localStorageService.GetItemAsync<T?>(Key, cancellationToken);
+    }
+
+
+    private T? _object;
+
+    public T? Object
+    {
+        get => _object; 
+        set => Set(ref _object, value);
+    }
+
+
+    protected virtual string Key => string.Empty;
+}

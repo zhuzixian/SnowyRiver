e7a8b2951be4dc706223f473eafbb6856ca374c9
Shady Joo <zhuzixian@gmail.com>
2025-11-05 16:34:42 +0800

    新增 RetryExecuteObject 类，支持异步重试策略
    更新项目版本号至 1.0.8，新增异步操作支持类 RetryExecuteObject。
    - 在 `SnowyRiver.Polly.csproj` 中更新版本号为 1.0.8。
    - 新增 `RetryExecuteObject.cs` 文件，提供基于 Polly 的异步重试支持。
      - 引入 `Nito.AsyncEx` 实现异步锁。
      - 定义两个异步方法，支持无返回值和带返回值的任务执行。
      - 使用 Polly 的重试策略和异步锁封装执行逻辑。

---
+++ src/Commons/SnowyRiver.Polly/RetryExecuteObject.cs
@@ -0,0 +1,22 @@
+﻿using Nito.AsyncEx;
+using Polly.Retry;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnowyRiver.Polly;
+public class RetryExecuteObject(AsyncRetryPolicy retryPolicy)
+{
+    private static readonly AsyncLock AsyncLocker = new();
+
+    protected async Task ExecuteAsync(Func<Task> action, CancellationToken cancellationToken = default)
+    {
+        await Executor.ExecuteAsync(retryPolicy, AsyncLocker, action, cancellationToken);
+    }
+
+    protected async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> task,
+        CancellationToken cancellationToken = default)
+    {
+        return await Executor.ExecuteAsync(retryPolicy, AsyncLocker, task, cancellationToken);
+    }
+}

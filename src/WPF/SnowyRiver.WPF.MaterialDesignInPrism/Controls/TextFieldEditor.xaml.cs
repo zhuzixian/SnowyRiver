256ec7488c4af2e31534ca658bb0761890acd4b4
zhuzixian <zhuzixian@gmail.com>
2025-11-10 20:33:31 +0800

    新增 TextFieldEditor 控件并更新项目版本
    新增了一个自定义控件 TextFieldEditor，包括 XAML 和代码隐藏文件，支持绑定标题、值和单位等属性，并应用了 MahApps 样式。更新项目版本至 9.0.43，目标框架升级为 net9.0-windows。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Controls/TextFieldEditor.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Controls;
+public partial class TextFieldEditor
+{
+    public TextFieldEditor()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+        nameof(Header), typeof(string), typeof(TextFieldEditor), new PropertyMetadata(default(string)));
+
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value), typeof(string), typeof(TextFieldEditor), new PropertyMetadata(default(string)));
+
+    public string Value
+    {
+        get => (string)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    public static readonly DependencyProperty UnitProperty = DependencyProperty.Register(
+        nameof(Unit), typeof(string), typeof(TextFieldEditor), new PropertyMetadata(default(string)));
+
+    public string Unit
+    {
+        get => (string)GetValue(UnitProperty);
+        set => SetValue(UnitProperty, value);
+    }
+
+    public static readonly DependencyProperty HeaderWidthProperty = DependencyProperty.Register(
+        nameof(HeaderWidth), typeof(double), typeof(TextFieldEditor), new PropertyMetadata(0d));
+
+
+    public double HeaderWidth
+    {
+        get => (double)GetValue(HeaderWidthProperty);
+        set => SetValue(HeaderWidthProperty, value);
+    }
+
+    public static readonly DependencyProperty UnitWidthProperty = DependencyProperty.Register(
+        nameof(UnitWidth), typeof(double), typeof(TextFieldEditor), new PropertyMetadata(0d));
+
+
+    public double UnitWidth
+    {
+        get => (double)GetValue(UnitWidthProperty);
+        set => SetValue(UnitWidthProperty, value);
+    }
+}

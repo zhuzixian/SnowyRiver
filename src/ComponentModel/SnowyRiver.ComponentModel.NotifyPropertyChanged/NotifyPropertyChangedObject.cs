f0658fe3dfec9fadc7aa88369d601847d28be8f4
Shady Joo <zhuzixian@gmail.com>
2025-11-11 13:46:08 +0800

    更新项目版本号并增强 Set<T> 方法的可扩展性
    更新 SnowyRiver.WorkFlows 和 NotifyPropertyChanged 项目版本号。
    将 NotifyPropertyChangedObject 类中的 Set<T> 方法定义为 virtual，
    允许子类重写以增强扩展性。

--- src/ComponentModel/SnowyRiver.ComponentModel.NotifyPropertyChanged/NotifyPropertyChangedObject.cs
+++ src/ComponentModel/SnowyRiver.ComponentModel.NotifyPropertyChanged/NotifyPropertyChangedObject.cs
@@ -20,7 +20,7 @@ public class NotifyPropertyChangedObject : INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    protected bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(field, value)) return false;
         field = value;

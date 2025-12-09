b5729643ae1b015ca6acbdcbad3a0878eb1c6cff
zhuzixian <zhuzixian@gmail.com>
2025-12-09 21:45:55 +0800

    重构Executor锁用法并更新项目版本号
    重构了 Executor.cs，简化并优化了锁的使用方式，支持传递 CancellationToken。同步和异步方法均采用 using var 语法，提升代码可读性和健壮性。同时，SnowyRiver.AsyncEx.csproj 和 SnowyRiver.Polly.csproj 的版本号分别提升至 10.0.1 和 10.0.2。

--- src/Commons/SnowyRiver.AsyncEx/Executor.cs
+++ src/Commons/SnowyRiver.AsyncEx/Executor.cs
@@ -8,33 +8,25 @@ public static class Executor
 {
     public static async Task ExecuteAsync(AsyncLock locker, Func<Task> action, CancellationToken cancellationToken = default)
     {
-        using (await locker.LockAsync())
-        {
-            await action();
-        }
+        using var lockerDisposable = await locker.LockAsync(cancellationToken);
+        await action();
     }
 
     public static async Task<TResult> ExecuteAsync<TResult>(AsyncLock locker, Func<Task<TResult>> task, CancellationToken cancellationToken = default)
     {
-        using (await locker.LockAsync())
-        {
-            return await task.Invoke();
-        }
+        using var lockerDisposable = await locker.LockAsync(cancellationToken);
+        return await task.Invoke();
     }
 
     public static void Execute(AsyncLock locker, Action action)
     {
-        using (locker.Lock())
-        {
-            action.Invoke();
-        }
+        using var lockerDisposable = locker.Lock();
+        action.Invoke();
     }
 
     public static TResult Execute<TResult>(SemaphoreSlim locker, Func<TResult> func)
     {
-        using (locker.Lock())
-        {
-            return func.Invoke();
-        }
+        using var lockerDisposable = locker.Lock();
+        return func.Invoke();
     }
 }

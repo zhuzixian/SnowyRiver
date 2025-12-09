386d263e59e4ab513d064898428f723bac4faca9
zhuzixian <zhuzixian@gmail.com>
2025-12-10 07:33:34 +0800

    重构异常扩展方法并升级部分依赖包
    重命名并优化 IsCanceledException 方法，支持递归检查 AggregateException，新增 IsNotCanceledException 方法及注释。项目及部分 NuGet 包版本升级。

--- src/Commons/SnowyRiver.Exceptions/Extensions.cs
+++ src/Commons/SnowyRiver.Exceptions/Extensions.cs
@@ -1,16 +1,44 @@
 ﻿using System.Threading.Tasks;
 using System;
+using System.Linq;
 
 namespace SnowyRiver.Exceptions;
 public static class Extensions
 {
-    public static bool IsTaskCanceledOrOperationCanceledExceptionException(this Exception e)
+    /// <summary>
+    /// 判断异常或其内部异常是TaskCanceledException或OperationCanceledException
+    /// </summary>
+    /// <param name="ex">要检查的异常</param>
+    /// <returns>如果是取消异常返回true，否则返回false</returns>
+    public static bool IsCanceledException(Exception ex)
     {
-        if (e is TaskCanceledException or OperationCanceledException)
+        if (ex == null) return false;
+
+        var current = ex;
+        while (current != null)
         {
-            return true;
+            switch (current)
+            {
+                case TaskCanceledException:
+                case OperationCanceledException:
+                case AggregateException aggregateEx when aggregateEx.InnerExceptions.Any(IsCanceledException):
+                    return true;
+                case AggregateException:
+                    return false;
+                default:
+                    current = current.InnerException;
+                    break;
+            }
         }
 
-        return e.InnerException != null && IsTaskCanceledOrOperationCanceledExceptionException(e.InnerException);
+        return false;
+    }
+
+    /// <summary>
+    /// 判断异常是否不是取消异常（与IsCanceledException相反）
+    /// </summary>
+    public static bool IsNotCanceledException(Exception ex)
+    {
+        return !IsCanceledException(ex);
     }
 }

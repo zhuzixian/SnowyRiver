15f8f104e80357f29fc555f5fe2e7cb0f6215a70
zhuzixian <zhuzixian@gmail.com>
2025-12-10 07:00:31 +0800

    重命名异常扩展方法并支持更多类型
    将扩展方法重命名为 IsTaskCanceledOrOperationCanceledExceptionException，并扩展其逻辑以同时判断 TaskCanceledException 和 OperationCanceledException。递归调用同步更新。项目版本号由 10.0.0 升级为 10.0.1。

--- src/Commons/SnowyRiver.Exceptions/Extensions.cs
+++ src/Commons/SnowyRiver.Exceptions/Extensions.cs
@@ -4,13 +4,13 @@ using System;
 namespace SnowyRiver.Exceptions;
 public static class Extensions
 {
-    public static bool IsTaskCanceledException(this Exception e)
+    public static bool IsTaskCanceledOrOperationCanceledExceptionException(this Exception e)
     {
-        if (e is TaskCanceledException)
+        if (e is TaskCanceledException or OperationCanceledException)
         {
             return true;
         }
 
-        return e.InnerException != null && IsTaskCanceledException(e.InnerException);
+        return e.InnerException != null && IsTaskCanceledOrOperationCanceledExceptionException(e.InnerException);
     }
 }

1dcfa20e111a2463e64e9203515ab602253e49e1
Shady Joo <zhuzixian@gmail.com>
2025-12-11 16:30:38 +0800

    重构集合扩展方法并新增EF表达式扩展
    将集合相关扩展方法迁移至 CollectionExtensions.cs，并调整命名空间。新增 SetPropertyCallsExtensions.cs，支持合并 EF Core 批量更新表达式。项目升级至 10.0.1，添加 Microsoft.EntityFrameworkCore 包引用。移除原 Extensions.cs 文件。

---
+++ src/DDD/SnowyRiver.Domain.Shared/Extensions/CollectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using SnowyRiver.ComponentModel.Interface;
+
+namespace SnowyRiver.Domain.Shared.Extensions;
+public static class CollectionExtensions
+{
+    extension<T>(Collection<T> source) where T : IHasSortId, new()
+    {
+        public void InsertPrevious(T? nullableSelectedItem)
+        {
+            if (nullableSelectedItem != null)
+            {
+                var selectedIndex = source.IndexOf(nullableSelectedItem);
+                source.Insert(selectedIndex, new T());
+                for (var i = selectedIndex; i < source.Count; i++)
+                {
+                    if (source[i] is { } t)
+                    {
+                        t.SortId = i + 1;
+                    }
+                }
+            }
+        }
+
+        public void InsertNext(T? nullableSelectedItem)
+        {
+            if (nullableSelectedItem != null)
+            {
+                var selectedIndex = source.IndexOf(nullableSelectedItem);
+                source.Insert(selectedIndex + 1, new T());
+                for (var i = selectedIndex; i < source.Count; i++)
+                {
+                    if (source[i] is { } t)
+                    {
+                        t.SortId = i + 1;
+                    }
+                }
+            }
+        }
+
+        public void Remove(T? nullableSelectedItem)
+        {
+            if (nullableSelectedItem is { } selectedStep)
+            {
+                var selectedIndex = source.IndexOf(selectedStep);
+                source.RemoveAt(selectedIndex);
+                for (var i = selectedIndex; i < source.Count; i++)
+                {
+                    if (source[i] is { } t)
+                    {
+                        t.SortId = i + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void FillSortId<T>(this IEnumerable<T> records, IQueryFilter filter)
+        where T:IHasSortId
+    {
+        var recordsArray = records as T[] ?? records.ToArray();
+        var baseSortId = (filter.PageIndex - 1) * filter.PageSize;
+        for (var i = 0; i < recordsArray.Length; i++)
+        {
+            recordsArray[i].SortId = baseSortId + i + 1;
+        }
+    }
+}
---
+++ src/DDD/SnowyRiver.Domain.Shared/Extensions/SetPropertyCallsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace SnowyRiver.Domain.Shared.Extensions;
+public static class SetPropertyCallsExtensions
+{
+    public static Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> Append<TEntity>(
+        this Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> first,
+        Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> second)
+    {
+        var replacedExpression = new ReplacingExpressionVisitor(second.Parameters, new[] { first.Body })
+            .Visit(second.Body);
+
+        var combinedLambda = Expression.Lambda<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>>(
+            replacedExpression,
+            first.Parameters
+        );
+
+        return combinedLambda;
+    }
+}

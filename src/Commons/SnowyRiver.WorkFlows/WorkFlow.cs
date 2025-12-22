a9a843a5ba22aa5216f41f95963f8c555b87ffe2
zhuzixian <zhuzixian@gmail.com>
2025-12-22 21:03:39 +0800

    新增泛型工作流与步骤基类，提升扩展性
    升级项目版本至 10.0.6。新增 WorkFlow<T>、WorkFlow<TWorkStep, T> 和 WorkStep<T> 泛型基类，增强工作流及步骤的泛型继承能力，便于后续扩展和自定义。

--- src/Commons/SnowyRiver.WorkFlows/WorkFlow.cs
+++ src/Commons/SnowyRiver.WorkFlows/WorkFlow.cs
@@ -1,6 +1,10 @@
 ﻿namespace SnowyRiver.WorkFlows;
 
 public class WorkFlow : WorkFlow<Guid, WorkState, Guid, WorkStep, WorkState, WorkFlow>;
+
+public class WorkFlow<T> : WorkFlow<Guid, WorkState, Guid, WorkStep, WorkState, T>
+    where T : WorkFlow<T>;
+
 public class WorkFlow<TWorkStep, T> : WorkFlow<Guid, WorkState, Guid, TWorkStep, WorkState, T>
     where TWorkStep : WorkStep<Guid, WorkState, TWorkStep>
     where T: WorkFlow<TWorkStep, T>;
--- src/Commons/SnowyRiver.WorkFlows/WorkStep.cs
+++ src/Commons/SnowyRiver.WorkFlows/WorkStep.cs
@@ -1,3 +1,6 @@
 ﻿namespace SnowyRiver.WorkFlows;
 
 public class WorkStep : WorkStep<Guid, WorkState, WorkStep>;
+
+public class WorkStep<T> : WorkStep<Guid, WorkState, T>
+    where T : WorkStep<T>;

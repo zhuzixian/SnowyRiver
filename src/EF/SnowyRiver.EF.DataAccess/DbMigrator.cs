dfcb568960f7430fe35af4376bd3b7306a09df04
Shady Joo <zhuzixian@gmail.com>
2026-01-12 17:26:13 +0800

    增强 DbMigrator 可扩展性并升级版本号
    扩展 DbMigrator，新增可重写的 PrepareForMigrateAsync 和 SeedDataAsync 方法，支持迁移前准备和数据种子操作。MigrateAsync 参数微调。项目版本号升级至 10.0.1。

--- src/EF/SnowyRiver.EF.DataAccess/DbMigrator.cs
+++ src/EF/SnowyRiver.EF.DataAccess/DbMigrator.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityFrameworkCore.UnitOfWork.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using SnowyRiver.EF.DataAccess.Abstractions;
 
 namespace SnowyRiver.EF.DataAccess;
 
 public class DbMigrator(IUnitOfWorkFactory unitOfWorkFactory): IDbMigrator
 {
-    public virtual async Task MigrateAsync(CancellationToken cancellationToken = default)
+    public virtual async Task MigrateAsync(CancellationToken cancellationToken = new CancellationToken())
     {
         using var unitOfWork = unitOfWorkFactory.Create();
         var dbContext = unitOfWork.DbContext;
+        await PrepareForMigrateAsync(unitOfWork, cancellationToken);
         await dbContext.Database.MigrateAsync(cancellationToken);
         await dbContext.Database.EnsureCreatedAsync(cancellationToken);
+        await SeedDataAsync(unitOfWork, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    protected virtual async Task PrepareForMigrateAsync(IUnitOfWork unitOfWork,
+        CancellationToken cancellationToken = default)
+    {
+        await Task.CompletedTask;
+    }
+
+    protected virtual async Task SeedDataAsync(IUnitOfWork unitOfWork, CancellationToken cancellationToken = default)
+    {
+        await Task.CompletedTask;
     }
 }

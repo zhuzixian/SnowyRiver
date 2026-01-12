0e72b1a86196e9dc0c41826e595622401e642833
Shady Joo <zhuzixian@gmail.com>
2026-01-12 16:59:51 +0800

    Permission增加Description

--- src/Accounts/SnowyRiver.Accounts.Domain/Entities/PermissionBase.cs
+++ src/Accounts/SnowyRiver.Accounts.Domain/Entities/PermissionBase.cs
@@ -7,6 +7,7 @@ public class Permission<TUser, TRole, TTeam, TPermission> : HasNameCreationTimeS
     where TRole : Role<TUser, TRole, TTeam, TPermission>
     where TPermission : Permission<TUser, TRole, TTeam, TPermission>
 {
-    public string Alias { get; set; } = string.Empty;
-    public List<TRole> Roles { get; set; }
+    public string? Alias { get; set; }
+    public string? Description { get; set; }
+    public List<TRole>? Roles { get; set; }
 }

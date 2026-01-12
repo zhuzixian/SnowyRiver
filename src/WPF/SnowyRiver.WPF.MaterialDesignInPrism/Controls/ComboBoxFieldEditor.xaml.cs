08f535d0cea930c796ad3d2286630008ac86b022
zhuzixian <zhuzixian@gmail.com>
2026-01-12 21:00:19 +0800

    字段编辑器控件重构，新增下拉选择支持
    重构字段编辑器，抽取通用依赖属性至 FieldEditor 基类，TextFieldEditor 和新建的 ComboBoxFieldEditor 继承该基类。统一样式资源，删除原 TextFieldEditor 样式字典，新增 FieldEditor 样式资源。ComboBoxFieldEditor 支持 ItemsSource、DisplayMemberPath、SelectedItem 等属性。项目版本号升级至 10.0.18。

---
+++ src/WPF/SnowyRiver.WPF.MaterialDesignInPrism/Controls/ComboBoxFieldEditor.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Windows;
+
+namespace SnowyRiver.WPF.MaterialDesignInPrism.Controls;
+public partial class ComboBoxFieldEditor
+{
+    public ComboBoxFieldEditor()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value), typeof(string), typeof(ComboBoxFieldEditor), new PropertyMetadata(default(string)));
+
+    public string Value
+    {
+        get => (string)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(
+        nameof(ItemsSource), typeof(IEnumerable), typeof(ComboBoxFieldEditor), new PropertyMetadata(default(IEnumerable)));
+
+    public IEnumerable ItemsSource
+    {
+        get => (IEnumerable)GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
+    }
+
+    public static readonly DependencyProperty DisplayMemberPathProperty = DependencyProperty.Register(
+        nameof(DisplayMemberPath), typeof(string), typeof(ComboBoxFieldEditor), new PropertyMetadata(default(string)));
+
+    public string DisplayMemberPath
+    {
+        get => (string)GetValue(DisplayMemberPathProperty);
+        set => SetValue(DisplayMemberPathProperty, value);
+    }
+
+    public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(
+        nameof(SelectedItem), typeof(object), typeof(ComboBoxFieldEditor), new PropertyMetadata(default(object)));
+    public object SelectedItem
+    {
+        get => GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
+}

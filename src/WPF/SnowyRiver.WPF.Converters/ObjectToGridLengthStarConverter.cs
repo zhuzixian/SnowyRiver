4283dc9c19303f57a3916caf6f8d0e5a6b8890ec
zhuzixian <zhuzixian@gmail.com>
2026-03-03 21:44:30 +0800

    新增 ObjectToGridLengthStarConverter 转换器类
    实现了将 int/double 转为 GridLength.Star 的 IValueConverter，便于 XAML 动态绑定 Star 单位的 GridLength。项目版本号升级至 10.0.1。

---
+++ src/WPF/SnowyRiver.WPF.Converters/ObjectToGridLengthStarConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SnowyRiver.WPF.Converters;
+
+public class ObjectToGridLengthStarConverter: IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return new GridLength(intValue, GridUnitType.Star);
+        }
+        
+        if (value is double doubleValue)
+        {
+            return new GridLength(doubleValue, GridUnitType.Star);
+        }
+
+        return new GridLength(1, GridUnitType.Star);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is GridLength { GridUnitType: GridUnitType.Star } gridLength)
+        {
+            return (int)gridLength.Value;
+        }
+
+        return 1;
+    }
+}

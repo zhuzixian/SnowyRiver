773fcb44033803e0086d690b6bf14faeeb222d70
Shady Joo <zhuzixian@gmail.com>
2026-03-24 12:05:58 +0800

    新增 String 扩展方法并升级项目版本
    升级项目版本至 10.0.2，移除 Extensions 文件夹引用。新增 StringExtensions.cs，基于 C# 12 扩展方法为 string 类型添加 IsNotNullOrWhiteSpace 和 IsNotNullOrEmpty 方法。

---
+++ src/Commons/SnowyRiver.Commons/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SnowyRiver.Commons.Extensions;
+
+public static class StringExtensions
+{
+    extension(string value)
+    {
+        public bool IsNotNullOrWhiteSpace()
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        public bool IsNotNullOrEmpty()
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+    }
+}

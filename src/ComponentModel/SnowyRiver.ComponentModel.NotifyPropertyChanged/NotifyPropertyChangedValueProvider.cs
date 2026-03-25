39e0359d74a0c1839e4cb98da20fd63790764b52
Shady Joo <zhuzixian@gmail.com>
2026-03-25 17:03:48 +0800

    新增通用属性变更值提供器及项目依赖
    升级项目版本至 10.0.14，新增对 SnowyRiver.Commons 的引用。添加 NotifyPropertyChangedValueProvider<T> 泛型类，实现 IValueProvider<T> 接口，支持属性变更通知。完善组件模型的通用值提供能力。

---
+++ src/ComponentModel/SnowyRiver.ComponentModel.NotifyPropertyChanged/NotifyPropertyChangedValueProvider.cs
@@ -0,0 +1,12 @@
+﻿using SnowyRiver.Commons.Abstractions;
+
+namespace SnowyRiver.ComponentModel.NotifyPropertyChanged;
+
+public class NotifyPropertyChangedValueProvider<T> : NotifyPropertyChangedObject,IValueProvider<T>
+{
+    public T? Value
+    {
+        get;
+        set => Set(ref field, value);
+    }
+}

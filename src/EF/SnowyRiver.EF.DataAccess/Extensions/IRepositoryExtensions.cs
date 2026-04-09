80497a39c95d5b175ab176a824bc22f2221a4b67
Shady Joo <zhuzixian@gmail.com>
2026-04-09 10:04:26 +0800

    新增 IRepository 扩展方法并升级项目版本号
    将项目版本从 10.0.2 升级至 10.0.3。新增 IRepositoryExtensions.cs，提供 FirstOrDefaultAsync、LastOrDefaultAsync、SingleOrDefaultAsync 和 SearchAsync 等异步扩展方法，支持通过表达式谓词简化常用仓储查询操作，提升代码复用性和可读性。

---
+++ src/EF/SnowyRiver.EF.DataAccess/Extensions/IRepositoryExtensions.cs
@@ -0,0 +1,39 @@
+﻿using EntityFrameworkCore.Repository.Interfaces;
+using System.Linq.Expressions;
+
+namespace SnowyRiver.EF.DataAccess.Extensions;
+
+public static class RepositoryExtensions
+{
+    extension<T>(IRepository<T> repository) where T : class
+    {
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate,
+            CancellationToken cancellationToken = default)
+        {
+            return await repository.FirstOrDefaultAsync(repository.MultipleResultQuery()
+                .AndFilter(predicate), cancellationToken);
+        }
+
+        public async Task<T> LastOrDefaultAsync(Expression<Func<T, bool>> predicate,
+            CancellationToken cancellationToken = default)
+        {
+            return await repository.LastOrDefaultAsync(repository.MultipleResultQuery()
+                .AndFilter(predicate), cancellationToken);
+        }
+
+        public async Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate,
+            CancellationToken cancellationToken = default)
+        {
+            return await repository.SingleOrDefaultAsync(repository.SingleResultQuery()
+                .AndFilter(predicate), cancellationToken);
+        }
+
+        public async Task<IList<T>> SearchAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
+        {
+            return await repository.SearchAsync(repository.MultipleResultQuery()
+                .AndFilter(predicate), cancellationToken);
+        }
+
+
+    }
+}

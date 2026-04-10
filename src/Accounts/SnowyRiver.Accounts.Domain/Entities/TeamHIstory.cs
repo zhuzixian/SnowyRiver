71283c32d4728b97a80da168a205937da255c032
Shady Joo <zhuzixian@gmail.com>
2026-04-10 09:58:54 +0800

    重构 TeamHistory 继承关系并升级版本号
    将 TeamHistory 从继承 EntityHistory 改为继承 UserEntityHistory，
    以增强其用户相关的历史记录功能。同时将项目版本号
    从 10.0.12 升级到 10.0.13，修正了命名空间引用。

--- src/Accounts/SnowyRiver.Accounts.Domain/Entities/TeamHIstory.cs
+++ src/Accounts/SnowyRiver.Accounts.Domain/Entities/TeamHIstory.cs
@@ -1,8 +1,7 @@
-﻿using SnowyRiver.Domain.Entities;
+﻿namespace SnowyRiver.Accounts.Domain.Entities;
 
-namespace SnowyRiver.Accounts.Domain.Entities;
-
-public class TeamHistory<TUser, TRole, TTeam, TPermission> : EntityHistory<TTeam, Guid>
+public class TeamHistory<TUser, TRole, TTeam, TPermission> 
+    : UserEntityHistory<TTeam, Guid, TUser, TRole, TTeam, TPermission>
     where TTeam : Team<TUser, TRole, TTeam, TPermission>
     where TUser : User<TUser, TRole, TTeam, TPermission>
     where TRole : Role<TUser, TRole, TTeam, TPermission>

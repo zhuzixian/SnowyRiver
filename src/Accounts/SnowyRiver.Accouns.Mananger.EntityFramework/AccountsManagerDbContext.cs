3c1483b0a41856fb40e39473f603c540fe538fa9
zhuzixian <zhuzixian@gmail.com>
2026-04-11 09:34:31 +0800

    重构 AccountsDbContext，抽取通用基类
    将 AccountsDbContext 的实体映射和配置逻辑提取到新建的 AccountsDbContextBase 基类中，提升代码复用性和可维护性。AccountsDbContext 和 AccountsManagerDbContext 均改为继承该基类。移除原有泛型实现，项目版本号升级至 10.0.7。

--- src/Accounts/SnowyRiver.Accouns.Mananger.EntityFramework/AccountsManagerDbContext.cs
+++ src/Accounts/SnowyRiver.Accouns.Mananger.EntityFramework/AccountsManagerDbContext.cs
@@ -3,6 +3,6 @@ using SnowyRiver.Accounts.Domain.Entities;
 using SnowyRiver.Accounts.EntityFramework;
 namespace SnowyRiver.Accounts.Manager.EntityFramework;
 public class AccountsManagerDbContext(DbContextOptions options) 
-    : AccountsDbContext<User, Role, Team, Permission, UserHistory, RoleHistory, TeamHistory, PermissionHistory>(options)
+    : AccountsDbContextBase<User, Role, Team, Permission, UserHistory, RoleHistory, TeamHistory, PermissionHistory>(options)
 {
 }
---
+++ src/Accounts/SnowyRiver.Accounts.EntityFramework/AccountsDbContextBase.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnowyRiver.Accounts.Domain.Entities;
+using SnowyRiver.EF;
+using System;
+
+namespace SnowyRiver.Accounts.EntityFramework;
+
+public class AccountsDbContextBase<TUser, TRole, TTeam, TPermission,
+    TUserHistory, TRoleHistory, TTeamHistory, TPermissionHistory>(DbContextOptions options) 
+    : SnowyRiverDbContext(options)
+    where TTeam : Team<TUser, TRole, TTeam, TPermission>
+    where TUser : User<TUser, TRole, TTeam, TPermission>
+    where TRole : Role<TUser, TRole, TTeam, TPermission>
+    where TPermission : Permission<TUser, TRole, TTeam, TPermission>
+    where TUserHistory: UserHistoryBase<TUser, TRole, TTeam, TPermission>
+    where TRoleHistory: RoleHistoryBase<TUser, TRole, TTeam, TPermission>
+    where TTeamHistory: TeamHistoryBase<TUser, TRole, TTeam, TPermission>
+    where TPermissionHistory: PermissionHistoryBase<TUser, TRole, TTeam, TPermission>
+{
+    protected override string DbTablePrefix => "Accounts_";
+
+    public DbSet<TUser> Users { get; set; }
+    public DbSet<TRole> Roles { get; set; }
+    public DbSet<TTeam> Teams { get; set; }
+    public DbSet<TPermission> Permissions { get; set; }
+
+    public DbSet<TUserHistory> UserHistories { get; set; }
+    public DbSet<TRoleHistory> RoleHistories { get; set; }
+    public DbSet<TTeamHistory> TeamHistories { get; set; }
+    public DbSet<TPermissionHistory> PermissionHistories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TPermission>(b =>
+        {
+            b.ToTable(DbTablePrefix + "Permissions", DbSchema);
+            b.HasKey(x => x.Id);
+        });
+
+        modelBuilder.Entity<TUser>(b =>
+        {
+            b.ToTable(DbTablePrefix + "Users", DbSchema);
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Name).IsRequired().HasMaxLength(64);
+            b.Property(x => x.Password).IsRequired().HasMaxLength(64);
+            b.Property(x => x.PasswordSalt).IsRequired().HasMaxLength(64);
+            b.HasMany(e => e.Roles).WithMany(e => e.Users)
+                .UsingEntity(DbTablePrefix + "UserRoles");
+        });
+
+
+        modelBuilder.Entity<TRole>(b =>
+        {
+            b.ToTable(DbTablePrefix + "Roles", DbSchema);
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Name).IsRequired().HasMaxLength(64);
+            b.HasMany(e => e.Permissions).WithMany(e => e.Roles)
+                .UsingEntity(DbTablePrefix + "RolePermissions");
+        });
+
+        modelBuilder.Entity<TTeam>(b =>
+        {
+            b.ToTable(DbTablePrefix + "Teams", DbSchema);
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Name).IsRequired().HasMaxLength(64);
+            b.HasMany(e => e.Users).WithMany(e => e.Teams)
+                .UsingEntity(DbTablePrefix + "UserTeams");
+        });
+
+        ConfigEntityHistory<TUser, Guid, TUserHistory>(modelBuilder, "UserHistories");
+        ConfigEntityHistory<TRole, Guid, TRoleHistory>(modelBuilder, "RoleHistories");
+        ConfigEntityHistory<TTeam, Guid,  TTeamHistory>(modelBuilder, "TeamHistories");
+        ConfigEntityHistory<TPermission, Guid, TPermissionHistory>(modelBuilder, "PermissionHistory");
+    }
+}

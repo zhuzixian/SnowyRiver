94e4c2222d5964ecd054501d1a7c2869bf1fd4c0
zhuzixian <zhuzixian@gmail.com>
2026-04-11 22:46:47 +0800

    新增 Prism.JsonConfigs 项目及 JSON 配置注册扩展
    新增 SnowyRiver.WPF.Prism.JsonConfigs 项目，支持通过扩展方法将基于 JsonConfiguration 的配置类从 JSON 文件加载并注册到 Prism 容器，便于 WPF 应用依赖注入配置。项目已加入解决方案并配置相关依赖。

---
+++ src/WPF/SnowyRiver.WPF.Prism.JsonConfigs/IContainerRegistryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using SnowyRiver.Configuration;
+
+namespace SnowyRiver.WPF.Prism.JsonConfigs;
+
+public static class IContainerRegistryExtensions
+{
+    public static IContainerRegistry RegisterJsonConfig<T>(
+        this IContainerRegistry containerRegistry,
+        string fileName)
+        where T: JsonConfiguration, new()
+    {
+        var options =  JsonConfiguration.Load<T>(fileName);
+        containerRegistry.RegisterInstance(options);
+        return containerRegistry;
+    }
+}

f23149e70551d8855bda3a4468a518822c1cc704
Shady Joo <zhuzixian@gmail.com>
2026-04-20 17:00:20 +0800

    新增导航属性批量附加扩展方法及版本升级
    在 EF 相关项目中升级版本号，新增 UnitOfWork 和 DbContext 的导航属性批量附加扩展方法，提升实体关系处理的便捷性和一致性。

---
+++ src/EF/SnowyRiver.EF.DataAccess/Extensions/UnitOfWorkExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkCore.UnitOfWork.Interfaces;
+
+namespace SnowyRiver.EF.DataAccess.Extensions;
+
+public static class UnitOfWorkExtensions
+{
+    public static void AttachStandardNavigations(this IUnitOfWork unitOfWork, object entity,
+        params string[] excludeProperties)
+    {
+        var type = entity.GetType();
+
+        var entities = excludeProperties
+            .Select(p => type.GetProperty(p)?.GetValue(entity))
+            .Where(e => e != null)
+            .ToArray();
+
+        foreach (var nav in entities)
+        {
+            unitOfWork.DbContext.Attach(nav!);
+        }
+    }
+}
---
+++ src/EF/SnowyRiver.EF/Extensions/DbContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace SnowyRiver.EF.Extensions;
+
+public static class DbContextExtensions
+{
+    extension(DbContext context)
+    {
+        /// <summary>
+        /// 批量附加导航属性实体，避免重复更新
+        /// </summary>
+        public void AttachIfNotNull(params object?[] entities)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity != null)
+                {
+                    context.Attach(entity);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 自动附加实体的所有导航属性
+        /// </summary>
+        public void AttachNavigationProperties<T>(T? entity, params string[] excludeProperties) 
+            where T : class
+        {
+            if (entity == null) return;
+
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p is { CanRead: true, PropertyType.IsClass: true } &&
+                            p.PropertyType != typeof(string) &&
+                            !excludeProperties.Contains(p.Name));
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(entity);
+                if (value != null && !context.Entry(value).IsKeySet)
+                {
+                    context.Attach(value);
+                }
+            }
+        }
+    }
+}

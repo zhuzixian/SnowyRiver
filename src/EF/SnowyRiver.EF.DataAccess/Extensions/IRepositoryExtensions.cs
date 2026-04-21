9b8ed24f40cf0244cc9520432e0c52101a757179
Shady Joo <zhuzixian@gmail.com>
2026-04-21 14:17:28 +0800

    重构仓储扩展方法参数并升级版本号
    重构 IRepositoryExtensions.cs，将 includes 参数由 params 数组改为可空单个 include 委托，简化方法签名和调用方式；调整相关查询方法以适配新参数形式。项目版本号升级至 10.0.10。

--- src/EF/SnowyRiver.EF.DataAccess/Extensions/IRepositoryExtensions.cs
+++ src/EF/SnowyRiver.EF.DataAccess/Extensions/IRepositoryExtensions.cs
@@ -9,60 +9,68 @@ public static class RepositoryExtensions
 {
     extension<T>(IRepository<T> repository) where T : class
     {
-        public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate,
-            CancellationToken cancellationToken = default,
-            params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+        public async Task<T?> FirstOrDefaultAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            CancellationToken cancellationToken = default)
         {
-            var query = repository.GetMultipleResultQuery(predicate, includes);
+            var query = repository.GetMultipleResultQuery(predicate, include);
             return await repository.FirstOrDefaultAsync(query, cancellationToken);
         }
 
-        public async Task<T?> LastOrDefaultAsync(Expression<Func<T, bool>> predicate,
-            CancellationToken cancellationToken = default,
-                params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+        public async Task<T?> LastOrDefaultAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            CancellationToken cancellationToken = default)
         {
-            var query = repository.GetMultipleResultQuery(predicate, includes);
+            var query = repository.GetMultipleResultQuery(predicate, include);
             return await repository.LastOrDefaultAsync(query, cancellationToken);
         }
 
-        public async Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate,
-            CancellationToken cancellationToken = default,
-            params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+        public async Task<T?> SingleOrDefaultAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            CancellationToken cancellationToken = default)
         {
-            var query = repository.GetSingleResultQuery(predicate, includes);
+            var query = repository.GetSingleResultQuery(predicate, include);
             return await repository.SingleOrDefaultAsync(query, cancellationToken);
         }
 
-        public async Task<IList<T>> SearchAsync(Expression<Func<T, bool>> predicate, 
-            CancellationToken cancellationToken = default,
-            params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+        public async Task<IList<T>> SearchAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            CancellationToken cancellationToken = default)
         {
-            var query = repository.GetMultipleResultQuery(predicate, includes);
+            var query = repository.GetMultipleResultQuery(predicate, include);
             return await repository.SearchAsync(query, cancellationToken);
         }
 
         public IMultipleResultQuery<T> GetMultipleResultQuery(
             Expression<Func<T, bool>> predicate,
-            params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null)
         {
-            var query = includes.Aggregate(
-                repository.MultipleResultQuery().AndFilter(predicate),
-                (current, include) =>
-                    current.Include(include));
+            var query = repository.MultipleResultQuery().AndFilter(predicate);
+
+            if (include != null)
+            {
+                query = query.Include(include);
+            }
+
             return query;
         }
 
         public ISingleResultQuery<T> GetSingleResultQuery(
             Expression<Func<T, bool>> predicate,
-            params Func<IQueryable<T>, IIncludableQueryable<T, object>>[] includes)
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null)
         {
-            var query = includes.Aggregate(
-                repository.SingleResultQuery().AndFilter(predicate),
-                (current, include) =>
-                    current.Include(include));
+            var query = repository.SingleResultQuery().AndFilter(predicate);
+
+            if (include != null)
+            {
+                query = query.Include(include);
+            }
+
             return query;
         }
     }
-
-    
 }

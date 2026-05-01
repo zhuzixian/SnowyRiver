225d620bbc44876562fa472db5da14ab8551c883
Shady Joo <zhuzixian@gmail.com>
2026-05-01 17:32:57 +0800

    新增 ObservableRect 泛型类并升级版本号
    在 csproj 文件中将版本号提升至 10.0.16。新增 ObservableRect.cs，包含 ObservableRect 及其泛型实现 ObservableRect<T>，支持属性变更通知，便于属性绑定和 UI 响应。

---
+++ src/ComponentModel/SnowyRiver.ComponentModel.NotifyPropertyChanged/ObservableRect.cs
@@ -0,0 +1,33 @@
+﻿namespace SnowyRiver.ComponentModel.NotifyPropertyChanged;
+
+public class ObservableRect : ObservableRect<int>
+{
+}
+
+public class ObservableRect<T> : NotifyPropertyChangedObject
+    where T : struct
+{
+    public T X
+    {
+        get;
+        set => Set(ref field, value);
+    }
+
+    public T Y
+    {
+        get;
+        set => Set(ref field, value);
+    }
+
+    public T Width
+    {
+        get;
+        set => Set(ref field, value);
+    }
+
+    public T Height
+    {
+        get;
+        set => Set(ref field, value);
+    }
+}

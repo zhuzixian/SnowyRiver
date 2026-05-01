19d3b1d841ad60742031c0373cbb78e5c5c2dc83
Shady Joo <zhuzixian@gmail.com>
2026-05-01 20:37:30 +0800

    新增泛型矩形结构体Rect并升级版本号
    升级 SnowyRiver.Domain 项目版本至 10.0.17。
    新增 Rect.cs，定义泛型记录类型 Rect<T> 及其 int 特化，包含 X、Y、Width、Height 属性。

---
+++ src/DDD/SnowyRiver.Domain/Entities/Rect.cs
@@ -0,0 +1,14 @@
+﻿namespace SnowyRiver.Domain.Entities;
+
+public record Rect : Rect<int>;
+
+public record Rect<T> where T : struct
+{
+    public T X { get; set; }
+
+    public T Y { get; set; }
+
+    public T Width { get; set; }
+
+    public T Height { get; set; }
+}

7ff2bce812c3a9b7725700564ff21afdf3a7872d
Shady Joo <zhuzixian@gmail.com>
2026-05-09 10:40:08 +0800

    重构Rect和Size，添加构造函数并升级版本
    重构了Rect和Size类，分别为其及其泛型版本添加了无参和带参构造函数，支持通过构造函数初始化属性。Rect和Size现在继承自各自的泛型实现。项目版本号从10.0.18升级到10.0.19。

--- src/DDD/SnowyRiver.Domain/Entities/Rect.cs
+++ src/DDD/SnowyRiver.Domain/Entities/Rect.cs
@@ -1,9 +1,31 @@
 ﻿namespace SnowyRiver.Domain.Entities;
 
-public record Rect : Rect<int>;
+public record Rect : Rect<int>
+{
+    public Rect()
+    {
+    }
+
+    public Rect(int x, int y, int width, int height) : base(x, y, width, height)
+    {
+    }
+}
 
 public record Rect<T>:Size<T> where T : struct
 {
+    public Rect()
+    {
+
+    }
+
+    public Rect(T x, T y, T width, T height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
     public T X { get; set; }
 
     public T Y { get; set; }
--- src/DDD/SnowyRiver.Domain/Entities/Size.cs
+++ src/DDD/SnowyRiver.Domain/Entities/Size.cs
@@ -1,9 +1,30 @@
 ﻿namespace SnowyRiver.Domain.Entities;
 
-public record Size : Size<int>;
+public record Size : Size<int>
+{
+    public Size(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public Size()
+    {
+    }
+}
 
 public record Size<T> where T : struct
 {
+    public Size(T width, T height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public Size()
+    {
+    }
+
     public T Width { get; set; }
 
     public T Height { get; set; }

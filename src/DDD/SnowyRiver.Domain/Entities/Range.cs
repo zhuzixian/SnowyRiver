25c191f47d5b6170318cd8d8193574998fcab919
Shady Joo <zhuzixian@gmail.com>
2026-05-12 17:04:53 +0800

    新增通用区间类型Range及版本号升级
    升级 SnowyRiver.Domain 版本至 10.0.20。新增 Range.cs，包含支持可空最小值和最大值的泛型区间类型 Range<T>，以及 int 和 double 类型的具体实现。

---
+++ src/DDD/SnowyRiver.Domain/Entities/Range.cs
@@ -0,0 +1,37 @@
+﻿namespace SnowyRiver.Domain.Entities;
+
+public class DoubleRange : Range<double>
+{
+    public DoubleRange()
+    {
+    }
+    public DoubleRange(double? min, double? max) : base(min, max)
+    {
+    }
+}
+
+public class Range: Range<int>
+{
+    public Range()
+    {
+    }
+    public Range(int? min, int? max) : base(min, max)
+    {
+    }
+}
+
+public class Range<T> where T : struct
+{
+    public Range()
+    {
+    }
+
+    public Range(T? min, T? max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    public T? Min { get; set; }
+    public T? Max { get; set; }
+}
